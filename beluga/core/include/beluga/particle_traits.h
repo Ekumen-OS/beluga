d38fd1964fa001b78ed69f34a002f1bae7da3b83
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-11-04 10:08:04 -0300

    Specialize traits for a pair of <state, weight>

--- beluga/core/include/beluga/particle_traits.h
+++ beluga/core/include/beluga/particle_traits.h
@@ -31,61 +31,80 @@ using decay_tuple_t = typename decay_tuple<T>::type;
 template <class T>
 struct particle_traits {};
 
+template <template <class...> class Pair, class State, class... Extra>
+struct particle_traits<Pair<State, double, Extra...>> {
+  using value_type = Pair<State, double>;
+
+  template <class T>
+  static constexpr decltype(auto) state(T&& particle) {
+    return std::get<0>(std::forward<T>(particle));
+  }
+
+  template <class T>
+  static constexpr decltype(auto) weight(T&& particle) {
+    return std::get<1>(std::forward<T>(particle));
+  }
+
+  static constexpr auto states_view() { return views::elements<0>; }
+
+  static constexpr auto weights_view() { return views::elements<1>; }
+};
+
 template <template <class...> class Tuple, class State, class... Extra>
 struct particle_traits<Tuple<State, double, std::size_t, Extra...>> {
   using value_type = Tuple<State, double, std::size_t>;
 
   template <class T>
-  static decltype(auto) state(T&& particle) {
+  static constexpr decltype(auto) state(T&& particle) {
     return std::get<0>(std::forward<T>(particle));
   }
 
   template <class T>
-  static decltype(auto) weight(T&& particle) {
+  static constexpr decltype(auto) weight(T&& particle) {
     return std::get<1>(std::forward<T>(particle));
   }
 
   template <class T>
-  static decltype(auto) cluster(T&& particle) {
+  static constexpr decltype(auto) cluster(T&& particle) {
     return std::get<2>(std::forward<T>(particle));
   }
 
-  static auto states_view() { return views::elements<0>; }
+  static constexpr auto states_view() { return views::elements<0>; }
 
-  static auto weights_view() { return views::elements<1>; }
+  static constexpr auto weights_view() { return views::elements<1>; }
 
-  static auto clusters_view() { return views::elements<2>; }
+  static constexpr auto clusters_view() { return views::elements<2>; }
 };
 
 template <class T>
-decltype(auto) state(T&& particle) {
+constexpr decltype(auto) state(T&& particle) {
   return particle_traits<decay_tuple_t<T>>::state(std::forward<T>(particle));
 }
 
 template <class T>
-decltype(auto) weight(T&& particle) {
+constexpr decltype(auto) weight(T&& particle) {
   return particle_traits<decay_tuple_t<T>>::weight(std::forward<T>(particle));
 }
 
 template <class T>
-decltype(auto) cluster(T&& particle) {
+constexpr decltype(auto) cluster(T&& particle) {
   return particle_traits<decay_tuple_t<T>>::cluster(std::forward<T>(particle));
 }
 
 namespace views {
 
 template <class T>
-auto states() {
+constexpr auto states() {
   return particle_traits<decay_tuple_t<T>>::states_view();
 }
 
 template <class T>
-auto weights() {
+constexpr auto weights() {
   return particle_traits<decay_tuple_t<T>>::weights_view();
 }
 
 template <class T>
-auto clusters() {
+constexpr auto clusters() {
   return particle_traits<decay_tuple_t<T>>::clusters_view();
 }
 
