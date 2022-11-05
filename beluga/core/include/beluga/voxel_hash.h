ada3752c3d9cdc28a66f7a6344cccac40a59de45
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-11-05 17:59:29 -0300

    Support arithmetic types

--- beluga/core/include/beluga/voxel_hash.h
+++ beluga/core/include/beluga/voxel_hash.h
@@ -15,7 +15,7 @@ struct voxel_hash {};
 template <template <class...> class Tuple, class... Types>
 struct voxel_hash<
     Tuple<Types...>,
-    std::enable_if_t<(std::is_floating_point_v<Types> && ...) && sizeof...(Types) <= 3, void>> {
+    std::enable_if_t<(std::is_arithmetic_v<Types> && ...) && sizeof...(Types) <= 3, void>> {
  public:
   constexpr std::size_t operator()(const Tuple<Types...>& tuple, double voxel_size = 1.) const {
     return hash_impl(tuple, voxel_size, std::make_index_sequence<sizeof...(Types)>());
