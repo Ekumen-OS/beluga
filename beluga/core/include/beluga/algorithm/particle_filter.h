1c270a06d66bcf75010e60bf39cee139ba5065d2
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-11-15 21:27:29 -0300

    Rename particle filter class and methods

--- beluga/core/include/beluga/algorithm/particle_filter.h
+++ beluga/core/include/beluga/algorithm/particle_filter.h
@@ -11,10 +11,11 @@
 namespace beluga {
 
 template <class Mixin, class Container>
-struct ParticleFilter : public Mixin {
+struct BootstrapParticleFilter : public Mixin {
  public:
   template <class... Args>
-  explicit ParticleFilter(Args&&... args) : Mixin(std::forward<Args>(args)...), particles_{this->self().max_samples()} {
+  explicit BootstrapParticleFilter(Args&&... args)
+      : Mixin(std::forward<Args>(args)...), particles_{this->self().max_samples()} {
     using particle_type = typename Container::value_type;
     auto first = std::begin(views::all(particles_));
     auto last =
@@ -25,23 +26,27 @@ struct ParticleFilter : public Mixin {
   auto particles() { return views::all(particles_); }
 
   void update() {
-    sample();
-    resample();
+    sampling();
+    importance_sampling();
+    resampling();
   }
 
  private:
   Container particles_;
 
-  void sample() {
+  void sampling() {
     auto&& states = views::states(particles_);
     ranges::transform(
         states, std::begin(states), [this](const auto& state) { return this->self().apply_motion(state); });
-    ranges::transform(states, std::begin(views::weights(particles_)), [this](const auto& state) {
+  }
+
+  void importance_sampling() {
+    ranges::transform(views::states(particles_), std::begin(views::weights(particles_)), [this](const auto& state) {
       return this->self().importance_weight(state);
     });
   }
 
-  void resample() {
+  void resampling() {
     auto new_particles = Container{this->self().max_samples()};
     auto first = std::begin(views::all(new_particles));
     auto last = ranges::copy(this->self().generate_samples(particles_) | this->self().take_samples(), first).out;
@@ -59,14 +64,14 @@ template <
     class Container = TupleVector<std::pair<State, double>>>
 struct MCL : public ciabatta::mixin<
                  MCL<MotionModel, SensorModel, State, Container>,
-                 ciabatta::curry<ParticleFilter, Container>::template mixin,
+                 ciabatta::curry<BootstrapParticleFilter, Container>::template mixin,
                  ciabatta::curry<NaiveGeneration>::template mixin,
                  ciabatta::curry<FixedResampling>::template mixin,
                  MotionModel,
                  SensorModel> {
   using ciabatta::mixin<
       MCL<MotionModel, SensorModel, State, Container>,
-      ciabatta::curry<ParticleFilter, Container>::template mixin,
+      ciabatta::curry<BootstrapParticleFilter, Container>::template mixin,
       ciabatta::curry<NaiveGeneration>::template mixin,
       ciabatta::curry<FixedResampling>::template mixin,
       // TODO(nahuel): Add estimate mixin, which given a set of particles
@@ -85,14 +90,14 @@ template <
     class Container = TupleVector<std::tuple<State, double, std::size_t>>>
 struct AMCL : public ciabatta::mixin<
                   AMCL<MotionModel, SensorModel, State, Container>,
-                  ciabatta::curry<ParticleFilter, Container>::template mixin,
+                  ciabatta::curry<BootstrapParticleFilter, Container>::template mixin,
                   ciabatta::curry<AdaptiveGeneration>::template mixin,
                   ciabatta::curry<KldResampling>::template mixin,
                   MotionModel,
                   SensorModel> {
   using ciabatta::mixin<
       AMCL<MotionModel, SensorModel, State, Container>,
-      ciabatta::curry<ParticleFilter, Container>::template mixin,
+      ciabatta::curry<BootstrapParticleFilter, Container>::template mixin,
       ciabatta::curry<AdaptiveGeneration>::template mixin,
       ciabatta::curry<KldResampling>::template mixin,
       // TODO(nahuel): Add estimate mixin, which given a set of particles
