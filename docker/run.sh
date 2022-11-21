2c27c1ebcb326e05b277fe13a8876ba83d5f1f5a
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-11-20 23:34:31 -0300

    Reorganize docker files (#17)

--- docker/run.sh
+++ docker/run.sh
@@ -4,10 +4,9 @@
 # Use `--build` the first time to build the docker image.
 
 set -o errexit
-cd $(dirname "$(readlink -f "$BASH_SOURCE")")/..
+cd $(dirname "$(readlink -f "$BASH_SOURCE")")
 
 [[ ! -z "${WITHIN_DEV}" ]] && echo "Already in the development environment!" && exit 1
 
-cd docker
 [[ "${1}" == "--build" ]] && docker-compose build dev
 USERID=$(id -u) GROUPID=$(id -g) docker-compose run --rm dev
