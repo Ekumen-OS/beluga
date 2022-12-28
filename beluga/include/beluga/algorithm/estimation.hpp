1d3d76da76ddcba0066080f13fe3bfb9778030a2
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2022-12-28 20:52:49 -0300

    Rewrite and add tests

--- beluga/include/beluga/algorithm/estimation.hpp
+++ beluga/include/beluga/algorithm/estimation.hpp
@@ -17,16 +17,26 @@
 
 #include <beluga/type_traits.hpp>
 #include <range/v3/view/common.hpp>
+#include <range/v3/view/transform.hpp>
 #include <sophus/se2.hpp>
 
 namespace beluga {
 
-template <class Derived, class OtherDerived>
-inline auto rowwise_covariance(
-    const Eigen::MatrixBase<Derived>& matrix,
-    const Eigen::MatrixBase<OtherDerived>& rowwise_mean) {
-  const auto centered = matrix.colwise() - rowwise_mean;
-  return (centered * centered.adjoint()) / (static_cast<typename Derived::Scalar>(matrix.cols() - 1));
+template <class Range, class Scalar>
+Eigen::Matrix2<Scalar> covariance(const Range& range, const Eigen::Vector2<Scalar>& mean) {
+  Eigen::Vector3<Scalar> coefficients = std::transform_reduce(
+      range.begin(), range.end(), Eigen::Vector3<Scalar>::Zero().eval(), std::plus{}, [mean](const auto& value) {
+        const auto centered = value - mean;
+        return Eigen::Vector3<Scalar>{
+            centered.x() * centered.x(),
+            centered.x() * centered.y(),
+            centered.y() * centered.y(),
+        };
+      });
+  coefficients /= (static_cast<Scalar>(range.size() - 1));
+  auto covariance_matrix = Eigen::Matrix2<Scalar>{};
+  covariance_matrix << coefficients(0), coefficients(1), coefficients(1), coefficients(2);
+  return covariance_matrix;
 }
 
 template <class Mixin>
@@ -36,16 +46,32 @@ class SimpleEstimation : public Mixin {
   explicit SimpleEstimation(Args&&... args) : Mixin(std::forward<Args>(args)...) {}
 
   [[nodiscard]] std::pair<Sophus::SE2d, Eigen::Matrix3d> estimated_pose() const {
-    auto&& poses = this->self().states() | ranges::view::common;
-    Eigen::Matrix4Xd matrix{4, poses.size()};
-    std::transform(poses.begin(), poses.end(), matrix.colwise().begin(), [](const auto& pose) {
-      return Eigen::Map<const Eigen::Vector4d>{pose.data()};
-    });
-    const Sophus::SE2d mean = Eigen::Map<const Sophus::SE2d>{matrix.rowwise().mean().eval().data()};
-    Eigen::Matrix3d covariance = Eigen::Matrix3d::Zero();
-    covariance.topLeftCorner<2, 2>() = rowwise_covariance(matrix.bottomRows<2>(), mean.translation());
-    covariance.coeffRef(2, 2) = -2.0 * std::log(mean.so2().unit_complex().norm());
-    return std::pair{mean, covariance};
+    const auto poses_view = this->self().states() | ranges::views::common;
+    const auto translation_view =
+        poses_view | ranges::views::transform([](const auto& pose) { return pose.translation(); });
+
+    // Compute the average of all the coefficients of the SE(2) group elements and
+    // construct a new SE(2) element.
+    const Eigen::Vector4d sum_vector = std::transform_reduce(
+        poses_view.begin(), poses_view.end(), Eigen::Vector4d::Zero().eval(), std::plus{},
+        [](const auto& pose) { return Eigen::Map<const Eigen::Vector4d>{pose.data()}; });
+    const Eigen::Vector4d mean_vector = sum_vector / static_cast<double>(poses_view.size());
+    Sophus::SE2d mean = Eigen::Map<const Sophus::SE2d>{mean_vector.data()};
+
+    // Compute the covariance of the translation part.
+    Eigen::Matrix3d covariance_matrix = Eigen::Matrix3d::Zero();
+    covariance_matrix.topLeftCorner<2, 2>() = covariance(translation_view, mean.translation());
+
+    // Compute the circular variance and re-normalize the rotation component.
+    if (mean.so2().unit_complex().norm() < std::numeric_limits<double>::epsilon()) {
+      // Handle the case where both averages are too close to zero.
+      covariance_matrix.coeffRef(2, 2) = std::numeric_limits<double>::infinity();
+      mean.so2() = Sophus::SO2d{0.0};
+    } else {
+      covariance_matrix.coeffRef(2, 2) = -2.0 * std::log(mean.so2().unit_complex().norm());
+      mean.so2().normalize();
+    }
+    return std::pair{mean, covariance_matrix};
   }
 };
 
