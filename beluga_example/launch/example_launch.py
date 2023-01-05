ad8e21f3ded9ced3828c488746522d98c6122846
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-01-05 14:52:04 -0300

    Add lint step to CI (#37)
    This patch adds a step to run pre-commit hooks in CI. Now the pipeline builds and tests beluga_amcl as well.
    
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga_example/launch/example_launch.py
+++ beluga_example/launch/example_launch.py
@@ -18,77 +18,77 @@ from ament_index_python.packages import get_package_share_directory
 
 from launch import LaunchDescription
 from launch.actions import GroupAction
-from launch_ros.actions import SetParameter
 from launch_ros.actions import Node
+from launch_ros.actions import SetParameter
 
 
 def generate_launch_description():
-    example_dir = get_package_share_directory("beluga_example")
+    example_dir = get_package_share_directory('beluga_example')
 
     load_nodes = GroupAction(
         actions=[
-            SetParameter("use_sim_time", True),
+            SetParameter('use_sim_time', True),
             Node(
-                package="rviz2",
-                executable="rviz2",
-                name="rviz2",
+                package='rviz2',
+                executable='rviz2',
+                name='rviz2',
                 arguments=[
-                    "--display-config",
-                    os.path.join(example_dir, "rviz", "rviz.rviz"),
+                    '--display-config',
+                    os.path.join(example_dir, 'rviz', 'rviz.rviz'),
                 ],
             ),
             Node(
-                package="nav2_map_server",
-                executable="map_server",
-                name="map_server",
-                output="screen",
+                package='nav2_map_server',
+                executable='map_server',
+                name='map_server',
+                output='screen',
                 respawn=True,
                 respawn_delay=2.0,
                 parameters=[
                     {
-                        "yaml_filename": os.path.join(
-                            example_dir, "maps", "turtlebot3_world.yaml"
+                        'yaml_filename': os.path.join(
+                            example_dir, 'maps', 'turtlebot3_world.yaml'
                         )
                     }
                 ],
-                arguments=["--ros-args", "--log-level", "info"],
+                arguments=['--ros-args', '--log-level', 'info'],
             ),
             Node(
-                package="beluga_amcl",
-                executable="amcl_node",
-                name="amcl",
-                output="screen",
+                package='beluga_amcl',
+                executable='amcl_node',
+                name='amcl',
+                output='screen',
                 respawn=True,
                 respawn_delay=2.0,
-                parameters=[os.path.join(example_dir, "config", "params.yaml")],
-                arguments=["--ros-args", "--log-level", "info"],
+                parameters=[os.path.join(example_dir, 'config', 'params.yaml')],
+                arguments=['--ros-args', '--log-level', 'info'],
             ),
             Node(
-                package="nav2_lifecycle_manager",
-                executable="lifecycle_manager",
-                name="lifecycle_manager_localization",
-                output="screen",
-                arguments=["--ros-args", "--log-level", "info"],
+                package='nav2_lifecycle_manager',
+                executable='lifecycle_manager',
+                name='lifecycle_manager_localization',
+                output='screen',
+                arguments=['--ros-args', '--log-level', 'info'],
                 parameters=[
-                    {"autostart": True},
-                    {"node_names": ["map_server", "amcl"]},
+                    {'autostart': True},
+                    {'node_names': ['map_server', 'amcl']},
                 ],
             ),
             Node(
-                package="flatland_server",
-                executable="flatland_server",
-                output="screen",
+                package='flatland_server',
+                executable='flatland_server',
+                output='screen',
                 parameters=[
                     {
-                        "world_path": os.path.join(
-                            example_dir, "worlds", "turtlebot3_world.yaml"
+                        'world_path': os.path.join(
+                            example_dir, 'worlds', 'turtlebot3_world.yaml'
                         )
                     },
-                    {"update_rate": 200.0},
-                    {"step_size": 0.005},
-                    {"show_viz": False},
-                    {"viz_pub_rate": 30.0},
-                    {"use_sim_time": True},
+                    {'update_rate': 200.0},
+                    {'step_size': 0.005},
+                    {'show_viz': False},
+                    {'viz_pub_rate': 30.0},
+                    {'use_sim_time': True},
                 ],
             ),
         ]
