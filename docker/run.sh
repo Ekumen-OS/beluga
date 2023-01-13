947ac0159bcb2bf86df6d20ac3ad337384732329
olmerg <olmerg@gmail.com>
2023-01-13 16:05:12 -0500

    Update run.sh
    Signed-off-by: olmerg <olmerg@gmail.com>

--- docker/run.sh
+++ docker/run.sh
@@ -9,4 +9,4 @@ cd $(dirname "$(readlink -f "$BASH_SOURCE")")
 [[ ! -z "${WITHIN_DEV}" ]] && echo "Already in the development environment!" && exit 1
 
 [[ "${1}" == "--build" ]] && docker-compose build dev
-USERID=$(id -u) GROUPID=$(id -g) docker compose run --rm dev
+USERID=$(id -u) GROUPID=$(id -g) docker-compose run --rm dev
