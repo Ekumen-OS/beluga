ec815410562287db39812c99bc4d931ea4d53047
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-01-18 09:11:21 -0300

    Refactor to simplify scripts

--- beluga/docs/generate_docs.sh
+++ beluga/docs/generate_docs.sh
@@ -2,16 +2,9 @@
 
 # Generate documentation using doxygen.
 
-# Get Doxyfile path.
-SCRIPT_PATH=$(dirname "$(readlink -f -- "$0")")
-
-# Set default output directory and convert to absolute path.
-DOXYGEN_OUTPUT_DIRECTORY=$(realpath ${DOXYGEN_OUTPUT_DIRECTORY:-"${SCRIPT_PATH}/../generated_docs"})
-DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT:-"html"}
-
-cd $SCRIPT_PATH
+cd $(dirname "$(readlink -f -- "$0")")
 
 exec \
-  env DOXYGEN_OUTPUT_DIRECTORY=${DOXYGEN_OUTPUT_DIRECTORY} \
-  env DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT} \
+  env DOXYGEN_OUTPUT_DIRECTORY=${DOXYGEN_OUTPUT_DIRECTORY:-"."} \
+  env DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT:-"html"} \
   doxygen ./Doxyfile
