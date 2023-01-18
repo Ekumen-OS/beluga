0ba7c7133f26735458cc622f52afb74f93cbbb01
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-01-17 22:33:20 -0300

    Add documentation build step to CI
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga/docs/generate_docs.sh
+++ beluga/docs/generate_docs.sh
@@ -1,3 +1,17 @@
 #!/bin/sh
+
+# Generate documentation using doxygen.
+
+# Get Doxyfile path.
 SCRIPT_PATH=$(dirname "$(readlink -f -- "$0")")
-cd $SCRIPT_PATH && doxygen ./Doxyfile && cd -
+
+# Set default output directory and convert to absolute path.
+DOXYGEN_OUTPUT_DIRECTORY=$(realpath ${DOXYGEN_OUTPUT_DIRECTORY:-"${SCRIPT_PATH}/../generated_docs"})
+DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT:-"html"}
+
+cd $SCRIPT_PATH
+
+exec \
+  env DOXYGEN_OUTPUT_DIRECTORY=${DOXYGEN_OUTPUT_DIRECTORY} \
+  env DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT} \
+  doxygen ./Doxyfile
