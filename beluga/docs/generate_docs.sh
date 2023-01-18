00ee2abba2be1830e3091708f4eac22d681ce547
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-01-18 09:43:51 -0300

    No need to pass env variables manually

--- beluga/docs/generate_docs.sh
+++ beluga/docs/generate_docs.sh
@@ -3,8 +3,4 @@
 # Generate documentation using doxygen.
 
 cd $(dirname "$(readlink -f -- "$0")")
-
-exec \
-  env DOXYGEN_OUTPUT_DIRECTORY=${DOXYGEN_OUTPUT_DIRECTORY:-"."} \
-  env DOXYGEN_HTML_OUTPUT=${DOXYGEN_HTML_OUTPUT:-"html"} \
-  doxygen ./Doxyfile
+doxygen ./Doxyfile
