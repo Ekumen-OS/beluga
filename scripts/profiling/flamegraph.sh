8e3d8a3a7ac942d0ced530ca6494d793631b9f23
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2023-02-07 16:58:58 -0300

    Clone FlameGraph repo in dockerfile
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- scripts/profiling/flamegraph.sh
+++ scripts/profiling/flamegraph.sh
@@ -1,13 +1,7 @@
 #!/bin/sh
 
-SCRIPT_DIR=$(cd $(dirname -- "$0") &> /dev/null && pwd)
-
 set -e
-if [ ! -d "$SCRIPT_DIR/FlameGraph" ]; then
-    echo "Cloning FlameGraph, this will cached for the next time you run the script"
-    cd $SCRIPT_DIR && git clone https://github.com/brendangregg/FlameGraph && cd -
-fi
 echo "Generating a flamegraph from recorded events, this may take long..."
 perf script > out.perf
-$SCRIPT_DIR/FlameGraph/stackcollapse-perf.pl out.perf > out.perf-folded
-$SCRIPT_DIR/FlameGraph/flamegraph.pl out.perf-folded > flamegraph.svg
+/opt/FlameGraph/stackcollapse-perf.pl out.perf > out.perf-folded
+/opt/FlameGraph/flamegraph.pl out.perf-folded > flamegraph.svg
