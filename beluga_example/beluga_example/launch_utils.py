e67c195067ee5996cf007aeec85e84d5a65d9a43
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2023-02-09 19:06:00 +0000

    * Add beluga_example package with launch utilities.   * Utility to access launch arguments early.   * Utility to declare launch arguments based on parameter file. * Make all beluga parameters configurable from launch file. * Make param file path configurable. * Be able to record /pose and /tf topics for postprocessing.
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

---
+++ beluga_example/beluga_example/launch_utils.py
@@ -0,0 +1,121 @@
+# Copyright 2023 Ekumen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+from typing import List
+
+from launch import LaunchDescription
+from launch import LaunchDescriptionSource
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.actions import OpaqueFunction
+import launch.logging
+from launch.utilities import perform_substitutions
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+from launch_ros.descriptions import Parameter
+
+import yaml
+
+
+def with_launch_arguments(arguments_to_declare: List[DeclareLaunchArgument]):
+    """Decorator to resolve launch arguments early."""
+
+    def decorator(get_launch_description_f):
+        def wrapper():
+            launch_configurations_for_arguments = [
+                LaunchConfiguration(arg.name) for arg in arguments_to_declare
+            ]
+
+            def action(context):
+                resolved_args = {
+                    perform_substitutions(
+                        context, config.variable_name
+                    ): config.perform(context)
+                    for config in launch_configurations_for_arguments
+                }
+                return [
+                    IncludeLaunchDescription(
+                        LaunchDescriptionSource(
+                            get_launch_description_f(**resolved_args)
+                        )
+                    )
+                ]
+
+            return LaunchDescription(
+                [
+                    *arguments_to_declare,
+                    OpaqueFunction(function=action),
+                ]
+            )
+
+        return wrapper
+
+    return decorator
+
+
+def get_node_with_arguments_declared_from_params_file(
+    *, params_file, name, namespace='/', **kwargs
+):
+    """
+    Declare arguments from param file.
+
+    Currently ROS 2 argument handling is a bit broken ...
+    If you specify a parameter file, there's no way to override the parameter value, except with other parameter file.
+
+    Some magic to be able to easily override parameter values specified in a file from launch ...
+    """
+    full_name = f'{namespace}/{name}'
+    full_name_without_starting_slash = full_name.lstrip('/')
+    full_name_with_slash = f'/{full_name_without_starting_slash}'
+    with open(params_file) as params_file:
+        params_file_dict = yaml.safe_load(params_file)
+    node_name_key = None
+    if full_name_with_slash in params_file_dict:
+        node_name_key = full_name_with_slash
+    if full_name_without_starting_slash in params_file_dict:
+        node_name_key = full_name_without_starting_slash
+    if node_name_key is None:
+        launch.logging.get_logger().warn(
+            f"parameters file has no parameters for node '{full_name_without_starting_slash}'"
+        )
+        return [Node(name=name, namespace=namespace, **kwargs)]
+    entries_for_node = params_file_dict[node_name_key]
+    if 'ros__parameters' not in entries_for_node:
+        launch.logging.get_logger().warn(
+            f"parameters file does not have a 'ros__parameters' entry for node '{full_name_without_starting_slash}'"
+        )
+        return [Node(name=name, namespace=namespace, **kwargs)]
+    params_dict = entries_for_node['ros__parameters']
+    actions = []
+    parameters = []
+    for param_name, param_value in params_dict.items():
+        actions.append(
+            DeclareLaunchArgument(
+                name=f'{full_name_without_starting_slash}.{param_name}',
+                default_value=str(param_value),
+            )
+        )
+        parameters.append(
+            Parameter(
+                name=param_name,
+                value=LaunchConfiguration(
+                    f'{full_name_without_starting_slash}.{param_name}'
+                ),
+            )
+        )
+    actions.append(
+        Node(name=name, namespace=namespace, parameters=parameters, **kwargs)
+    )
+    return actions
--- beluga_example/launch/example_rosbag_launch.py
+++ beluga_example/launch/example_rosbag_launch.py
@@ -13,10 +13,10 @@
 # limitations under the License.
 
 from pathlib import Path
+from typing import List
 
 from ament_index_python.packages import get_package_share_directory
 
-from launch import Action
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.actions import GroupAction
@@ -24,132 +24,93 @@ from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.actions import ExecuteProcess
 from launch.actions import Shutdown
-import launch.logging
-from launch.utilities.type_utils import normalize_typed_substitution
-from launch.utilities.type_utils import perform_typed_substitution
-from launch.substitutions import LaunchConfiguration
+from launch.utilities.type_utils import get_typed_value
 
-from launch_ros.actions import Node
 from launch_ros.actions import SetParameter
-from launch_ros.descriptions import Parameter
 
-import yaml
-
-
-class PlayBag(Action):
-    def __init__(self, *, start_paused, example_dir_path, condition=None, **kwargs):
-        self._start_paused = normalize_typed_substitution(start_paused, bool)
-        self._example_dir_path = example_dir_path
-        self._kwargs = kwargs
-        super().__init__(condition=condition)
-
-    def execute(self, context):
-        start_paused = perform_typed_substitution(context, self._start_paused, bool)
-        cmd = [
-            'ros2',
-            'bag',
-            'play',
-            str(self._example_dir_path / 'bags' / 'perfect_odometry'),
-            '--rate',
-            '3',
-        ]
-        if start_paused:
-            cmd.append('--start-paused')
-        return [
-            ExecuteProcess(
-                cmd=cmd, output='own_log', on_exit=[Shutdown()], **self._kwargs
-            )
-        ]
+from beluga_example.launch_utils import with_launch_arguments
+from beluga_example.launch_utils import (
+    get_node_with_arguments_declared_from_params_file,
+)
 
 
-def get_declare_arguments_and_parameters_from_yaml_file(params_file, node_name):
-    """
-    Declare arguments from param file.
-
-    Currently ROS 2 argument handling is a bit broken ...
-    If you specify a parameter file, there's no way to override the parameter value, except with other parameter file.
-
-    Some magic to be able to easily override parameter values specified in a file from launch ...
-    """
-    node_name_without_starting_slash = node_name.lstrip('/')
-    node_name_with_slash = f'/{node_name_without_starting_slash}'
-    params_file_dict = yaml.safe_load(params_file)
-    node_name_key = None
-    if node_name_with_slash in params_file_dict:
-        node_name_key = node_name_with_slash
-    if node_name_without_starting_slash in params_file_dict:
-        node_name_key = node_name_without_starting_slash
-    if node_name_key is None:
-        launch.logging.get_logger().warn(
-            f"parameters file has no parameters for node '{node_name}'"
-        )
-        return [], []
-    entries_for_node = params_file_dict[node_name_key]
-    if 'ros__parameters' not in entries_for_node:
-        launch.logging.get_logger().warn(
-            f"parameters file does not have a 'ros__parameters' entry for node '{node_name}'"
-        )
-        return [], []
-    params_dict = entries_for_node['ros__parameters']
-    declared_launch_arguments = []
-    parameters = []
-    for name, value in params_dict.items():
-        declared_launch_arguments.append(
-            DeclareLaunchArgument(
-                name=f'{node_name}.{name}',
-                default_value=str(value),
-            )
-        )
-        parameters.append(
-            Parameter(
-                name=name,
-                value=LaunchConfiguration(f'{node_name}.{name}'),
-            )
-        )
-    return declared_launch_arguments, parameters
+def get_launch_arguments():
+    example_dir_path = Path(get_package_share_directory('beluga_example'))
+    params_file_path = example_dir_path / 'config' / 'params.yaml'
+    return [
+        DeclareLaunchArgument(
+            name='package',
+            default_value='beluga_amcl',
+            description='Package that provides the localization node to launch.',
+            choices=['beluga_amcl', 'nav2_amcl'],
+        ),
+        DeclareLaunchArgument(
+            name='node',
+            default_value='amcl_node',
+            description='Localization node to launch.',
+        ),
+        DeclareLaunchArgument(
+            name='prefix',
+            default_value='',
+            description='Set of commands/arguments to preceed the node command (e.g. "timem --").',
+        ),
+        DeclareLaunchArgument(
+            name='start_paused',
+            default_value='False',
+            description='Start the rosbag player in a paused state.',
+        ),
+        DeclareLaunchArgument(
+            name='amcl_parameters_file',
+            default_value=str(params_file_path),
+            description='Parameters file to be used to run amcl',
+        ),
+        DeclareLaunchArgument(
+            name='record_bag',
+            default_value='False',
+            description='If to record a bagfile or not',
+        ),
+        DeclareLaunchArgument(
+            name='topics_to_record',
+            default_value='[/tf, /pose]',
+            description='If to record a bagfile or not',
+        ),
+    ]
 
 
-def generate_launch_description():
+@with_launch_arguments(get_launch_arguments())
+def generate_launch_description(
+    package,
+    node,
+    prefix,
+    start_paused,
+    amcl_parameters_file,
+    record_bag,
+    topics_to_record,
+):
     example_dir_path = Path(get_package_share_directory('beluga_example'))
 
-    package = LaunchConfiguration('package')
-    node = LaunchConfiguration('node')
-    prefix = LaunchConfiguration('prefix')
-    start_paused = LaunchConfiguration('start_paused')
+    start_paused = get_typed_value(start_paused, bool)
+    record_bag = get_typed_value(record_bag, bool)
+    topics_to_record = get_typed_value(topics_to_record, List[str])
+    bag_play_cmd = [
+        'ros2',
+        'bag',
+        'play',
+        str(example_dir_path / 'bags' / 'perfect_odometry'),
+        '--rate',
+        '3',
+    ]
+    if start_paused:
+        bag_play_cmd.append('--start-paused')
 
-    package_launch_arg = DeclareLaunchArgument(
-        name='package',
-        default_value='beluga_amcl',
-        description='Package that provides the localization node to launch.',
-        choices=['beluga_amcl', 'nav2_amcl'],
-    )
-
-    node_launch_arg = DeclareLaunchArgument(
-        name='node',
-        default_value='amcl_node',
-        description='Localization node to launch.',
-    )
-
-    prefix_launch_arg = DeclareLaunchArgument(
-        name='prefix',
-        default_value='',
-        description='Set of commands/arguments to preceed the node command (e.g. "timem --").',
-    )
-
-    start_paused_launch_arg = DeclareLaunchArgument(
-        name='start_paused',
-        default_value='False',
-        description='Start the rosbag player in a paused state.',
-    )
-
-    node_name = 'amcl'
-    params_file_path = example_dir_path / 'config' / 'params.yaml'
-    with params_file_path.open() as params_file:
-        (
-            amcl_params_args,
-            amcl_params,
-        ) = get_declare_arguments_and_parameters_from_yaml_file(params_file, node_name)
-
+    other_nodes = []
+    if record_bag:
+        other_nodes.append(
+            ExecuteProcess(
+                cmd=['ros2', 'bag', 'record', *topics_to_record],
+                output='own_log',
+            )
+        )
     load_nodes = GroupAction(
         actions=[
             SetParameter('use_sim_time', True),
@@ -160,27 +121,23 @@ def generate_launch_description():
                     )
                 )
             ),
-            PlayBag(start_paused=start_paused, example_dir_path=example_dir_path),
-            Node(
+            ExecuteProcess(
+                cmd=bag_play_cmd,
+                output='own_log',
+                on_exit=[Shutdown()],
+            ),
+            *get_node_with_arguments_declared_from_params_file(
                 package=package,
                 executable=node,
                 namespace='',
                 name='amcl',
                 output='screen',
-                parameters=amcl_params,
                 arguments=['--ros-args', '--log-level', 'info'],
                 prefix=prefix,
+                params_file=amcl_parameters_file,
             ),
+            *other_nodes,
         ]
     )
 
-    return LaunchDescription(
-        [
-            package_launch_arg,
-            node_launch_arg,
-            prefix_launch_arg,
-            start_paused_launch_arg,
-            *amcl_params_args,
-            load_nodes,
-        ]
-    )
+    return LaunchDescription([load_nodes])
