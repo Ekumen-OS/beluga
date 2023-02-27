bc24c852976869c64a5c37534bada98b1dc78527
Ivan Santiago Paunovic <ivanpauno@gmail.com>
2023-02-27 17:48:48 +0000

    * Move benchmark/profiling scripts and docs to new package. * Add script to plot results comparision.
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@gmail.com>

---
+++ beluga_benchmark/beluga_benchmark/parameterized_run_compare.py
@@ -0,0 +1,137 @@
+# Copyright 2023 Ekumen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import argparse
+from pathlib import Path
+import re
+
+from evo import main_ape
+from evo.tools import file_interface
+import matplotlib.pyplot as plt
+from rosbag2_py._info import Info as Bag2Info
+import pandas as pd
+
+from . import timem
+from .exceptions import ScriptError
+
+DIR_NAME_PATTERN = 'benchmark_([0-9]+)_particles_output'
+SUPPORTED_TOPIC_NAMES = ('/pose', '/amcl_pose')
+EVO_RESULTS_FILE_NAME = 'evo_results.zip'
+
+
+def get_bag_est_topic(bag_path: Path):
+    metadata = Bag2Info().read_metadata(str(bag_path), '')
+    for topic_name in SUPPORTED_TOPIC_NAMES:
+        if any(
+            topic_info.topic_metadata.name == topic_name
+            for topic_info in metadata.topics_with_message_count
+        ):
+            return topic_name
+    raise ScriptError(
+        f'Estimate pose topic was not found in bag file, expected names: {SUPPORTED_TOPIC_NAMES}'
+    )
+
+
+def run_evo_ape(dir_path: Path):
+    evo_results_path = dir_path / EVO_RESULTS_FILE_NAME
+    if evo_results_path.exists():
+        return
+    bag_path = dir_path / 'rosbag'
+    est_topic = get_bag_est_topic(bag_path)
+    arg_parser = main_ape.parser()
+    args = arg_parser.parse_args(
+        [
+            'bag2',
+            str(bag_path),
+            '/odometry/ground_truth',
+            est_topic,
+            '--save_results',
+            str(evo_results_path.resolve()),
+        ]
+    )
+    main_ape.run(args)
+
+
+def read_evo_stats(zip_file_path):
+    # keys: "rmse", "mean", "median", "std", "min", "max", "sse"
+    return file_interface.load_res_file(zip_file_path).stats
+
+
+def create_parameterized_series(results_path: Path):
+    particles = []
+    peak_rss = []
+    cpu_usage = []
+    ape_rmse = []
+    ape_mean = []
+    ape_median = []
+    ape_max = []
+
+    for dir in results_path.iterdir():
+        if not dir.is_dir():
+            continue
+        m = re.match(DIR_NAME_PATTERN, dir.name)
+        if not m:
+            continue
+        particles.append(int(m[1]))
+        timem_output = timem.read_timem_output(dir)
+        rss, cpu = timem.get_timem_metrics_values(timem_output)
+        peak_rss.append(rss)
+        cpu_usage.append(cpu)
+        run_evo_ape(dir)
+        evo_results_path = dir / EVO_RESULTS_FILE_NAME
+        stats = read_evo_stats(evo_results_path)
+        ape_rmse.append(stats["rmse"])
+        ape_max.append(stats["max"])
+        ape_mean.append(stats["mean"])
+        ape_median.append(stats["median"])
+    return (
+        pd.DataFrame(
+            {
+                'particles_n': particles,
+                'peak_rss': peak_rss,
+                'cpu_usage': cpu_usage,
+                'ape_rmse': ape_rmse,
+                'ape_mean': ape_mean,
+                'ape_median': ape_median,
+                'ape_max': ape_max,
+            }
+        )
+        .set_index('particles_n')
+        .sort_index()
+    )
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        description='Script to compare parameterized run results of beluga with another'
+        ' implementation (e.g. nav2_amcl, another beluga version, etc).'
+    )
+    arg_parser.add_argument(
+        '--beluga-results',
+        '-b',
+        type=Path,
+        help='Folder with parameterized beluga benchmark results',
+    )
+    arg_parser.add_argument(
+        '--other-results',
+        '-o',
+        type=Path,
+        help='Folder with parameterized benchmark results for the other implementation',
+    )
+    args = arg_parser.parse_args()
+    beluga_series = create_parameterized_series(args.beluga_results)
+    other_series = create_parameterized_series(args.other_results)
+    ax = beluga_series.plot(subplots=True, color='red')
+    other_series.plot(subplots=True, ax=ax, color='blue')
+    plt.show()
--- beluga_benchmark/beluga_benchmark/timem.py
+++ beluga_benchmark/beluga_benchmark/timem.py
@@ -1,5 +1,3 @@
-#!/usr/bin/env python3
-#
 # Copyright 2023 Ekumen, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -14,39 +12,48 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from pathlib import Path
-import sys
-
+import argparse
 import json
+from pathlib import Path
 
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
+
+from .exceptions import ScriptError
+
+DEFAULT_FILE_NAME = "timem-output.json"
+
+
+def read_timem_output(dir_path: Path):
+    file = dir_path / DEFAULT_FILE_NAME
+    try:
+        with open(file, mode='r') as f:
+            output = json.load(f)
+    except (FileExistsError, FileNotFoundError) as ex:
+        raise ScriptError(f"Failed to open file '{file.absolute()}': {ex}")
+    return output['timemory']['timem'][0]
 
 
 def get_value_with_unit(data):
     return f"{data['value']:.2f}{data['unit_repr']}"
 
 
-def get_timem_metrics(pathlike):
-    path = Path(pathlike)
-    name = path.name
-    file = path / 'timem-output.json'
-    try:
-        with open(file, mode='r') as f:
-            output = json.load(f)
-    except (FileExistsError, FileNotFoundError) as ex:
-        print(f"Failed to open file '{file.absolute()}': {ex}", file=sys.stderr)
-        return 1
-    output = output['timemory']
-    output = output['timem'][0]
-    data = output['peak_rss']
-    peak_rss_str = get_value_with_unit(data)
-    data = output['cpu_util']
-    cpu_usage_str = get_value_with_unit(data)
+def get_timem_metrics(output):
+    return output['peak_rss'], output['cpu_util']
+
+
+def get_timem_metrics_values(output):
+    return tuple(data['value'] for data in get_timem_metrics(output))
+
+
+def get_timem_metrics_with_units(output):
+    return tuple(get_value_with_unit(data) for data in get_timem_metrics(output))
+
+
+def create_timeseries(output):
     data = output['wall_clock']
     elapsed_time = data['value']
-    elapsed_time_str = get_value_with_unit(data)
     time = np.arange(0.0, elapsed_time, 0.2)
     wall_clock_to_time_offset = output['history'][0]['wall_clock']['value']
     wall_clock_to_time_scale = elapsed_time / (
@@ -66,20 +73,26 @@ def get_timem_metrics(pathlike):
     series = pd.DataFrame(
         {'time': time, 'rss': rss, 'virtual_memory': virtual_memory}
     ).set_index('time')
+    return series
+
+
+def main():
+    arg_parser = argparse.ArgumentParser(
+        description='Script to postprocess timem results.'
+    )
+    arg_parser.add_argument(
+        'dir', help='Directory with timem-output.json file', type=Path
+    )
+    args = arg_parser.parse_args()
+    dir_path = Path(args.dir)
+    name = dir_path.name
+    output = read_timem_output(dir_path)
+    peak_rss_str, cpu_usage_str = get_timem_metrics_with_units(output)
+    series = create_timeseries(output)
+    elapsed_time_str = get_value_with_unit(output['wall_clock'])
     print(f"timem metrics for run '{name}':")
     print(f"\telapsed_time: {elapsed_time_str}")
     print(f"\tcpu_usage: {cpu_usage_str}")
     print(f"\tpeak_rss: {peak_rss_str}")
     series.plot(subplots=True)
     plt.show()
-
-
-if __name__ == '__main__':
-    import argparse
-
-    arg_parser = argparse.ArgumentParser(
-        description='Script to postprocess timem results.'
-    )
-    arg_parser.add_argument('dir', help='Directory with timem-output.json file')
-    args = arg_parser.parse_args()
-    sys.exit(get_timem_metrics(args.dir))
