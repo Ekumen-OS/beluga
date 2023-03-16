d8d86cd34c6210863973eb96c9fb0a88a9e977f8
Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>
2023-03-16 16:17:39 -0300

    Remove unnecessary code
    Signed-off-by: Ivan Santiago Paunovic <ivanpauno@ekumenlabs.com>

--- beluga_amcl/src/amcl_node.cpp
+++ beluga_amcl/src/amcl_node.cpp
@@ -819,7 +819,6 @@ void AmclNode::laser_callback(
         static_cast<float>(get_parameter("laser_min_range").as_double()),
         static_cast<float>(get_parameter("laser_max_range").as_double())));
     particle_filter_->importance_sample(exec_policy);
-    auto format = beluga_amcl::utils::make_eigen_comma_format();
     const auto time3 = std::chrono::high_resolution_clock::now();
     particle_filter_->resample();
     const auto time4 = std::chrono::high_resolution_clock::now();
