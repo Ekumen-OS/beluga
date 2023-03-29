440ac7fcbaee8ffc9706aca648c1b13265399262
Olmer Garcia-Bedoya <olmerg@ekumenlabs.com>
2023-03-29 16:20:50 -0500

    olmerg/ reinitialize_global_localization

--- beluga_amcl/src/amcl_node.cpp
+++ beluga_amcl/src/amcl_node.cpp
@@ -526,6 +526,14 @@ AmclNode::AmclNode(const rclcpp::NodeOptions & options)
 
   {
     auto descriptor = rcl_interfaces::msg::ParameterDescriptor();
+    descriptor.description = "Requires that AMCL is provided an initial pose either"
+      "via topic or initial_pose* parameter when reset. Otherwise, by default AMCL will use the last known pose to initialize";
+    descriptor.read_only = true;
+    declare_parameter("always_reset_initial_pose", false, descriptor);
+  }
+
+  {
+    auto descriptor = rcl_interfaces::msg::ParameterDescriptor();
     descriptor.read_only = true;
     descriptor.description =
       "Execution policy used to process particles [seq, par].";
@@ -569,6 +577,10 @@ AmclNode::CallbackReturn AmclNode::on_configure(const rclcpp_lifecycle::State &)
   pose_pub_ = create_publisher<geometry_msgs::msg::PoseWithCovarianceStamped>(
     "pose",
     rclcpp::SystemDefaultsQoS());
+
+  if (get_parameter("always_reset_initial_pose").as_bool()) {
+    initial_pose_is_known_ = false;
+  }
 
   return CallbackReturn::SUCCESS;
 }
