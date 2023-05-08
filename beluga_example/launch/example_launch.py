3b3f64ecd7e7871f1ca235581d3b3d0550396881
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-05-08 16:43:18 -0300

    Address code review comment
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga_example/launch/example_launch.py
+++ beluga_example/launch/example_launch.py
@@ -1,4 +1,4 @@
-# Copyright 2023 Ekumen, Inc.
+# Copyright 2022-2023 Ekumen, Inc.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
