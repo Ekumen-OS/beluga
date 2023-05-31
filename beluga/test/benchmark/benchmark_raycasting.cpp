9436030cb81f00d71f07bec33c5661d2c6896bcd
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-05-31 17:39:38 -0300

    Extend raycast microbenchmarks (#201)
    Related to #167.
    
    This patch extends existing microbenchmarks to use as reference to
    compare the performance of the raycast and occupancy grid
    implementation.
    
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga/test/benchmark/benchmark_raycasting.cpp
+++ beluga/test/benchmark/benchmark_raycasting.cpp
@@ -17,6 +17,7 @@
 #include <benchmark/benchmark.h>
 
 #include <beluga/algorithm/raycasting.hpp>
+#include <beluga/test/raycasting.hpp>
 #include <beluga/test/static_occupancy_grid.hpp>
 
 #include <range/v3/range/conversion.hpp>
@@ -50,23 +51,124 @@ BENCHMARK_CAPTURE(BM_Bresenham2i, Modified, beluga::Bresenham2i::kModified)
 
 using beluga::testing::StaticOccupancyGrid;
 
+template <std::size_t Rows, std::size_t Cols>
+class BaselineGrid : public beluga::BaseOccupancyGrid2<BaselineGrid<Rows, Cols>> {
+ public:
+  struct ValueTraits {
+    [[nodiscard]] bool is_free(bool value) const { return !value; }
+    [[nodiscard]] bool is_unknown(bool) const { return false; }
+    [[nodiscard]] bool is_occupied(bool value) const { return value; }
+  };
+
+  explicit BaselineGrid(std::initializer_list<bool>, double resolution) : resolution_{resolution} {
+    std::fill(std::begin(data()), std::end(data()), false);
+  }
+
+  [[nodiscard]] const Sophus::SE2d& origin() const { return origin_; }
+
+  [[nodiscard]] auto& data() { return grid_; }
+  [[nodiscard]] const auto& data() const { return grid_; }
+  [[nodiscard]] std::size_t size() const { return grid_.size(); }
+
+  [[nodiscard]] std::size_t width() const { return Cols; }
+  [[nodiscard]] std::size_t height() const { return Rows; }
+  [[nodiscard]] double resolution() const { return resolution_; }
+
+  [[nodiscard]] auto value_traits() const { return ValueTraits{}; }
+
+ private:
+  double resolution_;
+  Sophus::SE2d origin_;
+  std::array<bool, Rows * Cols> grid_;
+  static constexpr std::size_t kWidth = Cols;
+  static constexpr std::size_t kHeight = Rows;
+};
+
+const auto kBearingAngles = std::array{
+    0.,                                    // Horizontal
+    Sophus::Constants<double>::pi() / 2.,  // Vertical
+    Sophus::Constants<double>::pi() / 4.,  // Diagonal
+};
+
+const auto kBearingLabels = std::array{
+    "Horizontal",
+    "Vertical",
+    "Diagonal",
+};
+
+template <template <std::size_t, std::size_t> class Grid>
+void BM_RayCasting2d_BaselineRaycast(benchmark::State& state) {
+  constexpr double kMaxRange = 100.0;
+  constexpr double kResolution = 0.05;
+
+  const auto bearing_index = static_cast<std::size_t>(state.range(0));
+  const auto n = static_cast<int>(state.range(1));
+  Grid<1280, 1280> grid{{}, kResolution};
+  grid.data()[grid.index_at(n, n)] = true;
+  grid.data()[grid.index_at(0, n)] = true;
+  grid.data()[grid.index_at(n, 0)] = true;
+
+  const auto source_pose = Eigen::Vector2d{0., 0.};
+  const auto beam_bearing = Sophus::SO2d{kBearingAngles.at(bearing_index)};
+
+  const auto source = grid.cell_near(source_pose);
+  const auto target = grid.cell_near(source_pose + kMaxRange * beam_bearing.unit_complex());
+
+  for (auto _ : state) {
+    benchmark::DoNotOptimize(beluga::testing::raycast(grid, source, target));
+  }
+  state.SetComplexityN(n);
+  state.SetLabel(kBearingLabels.at(bearing_index));
+}
+
+BENCHMARK_TEMPLATE(BM_RayCasting2d_BaselineRaycast, BaselineGrid)
+    ->ArgsProduct({
+        {0, 1, 2},
+        {128, 256, 512, 1024},
+    })
+    ->Complexity();
+
+BENCHMARK_TEMPLATE(BM_RayCasting2d_BaselineRaycast, StaticOccupancyGrid)
+    ->ArgsProduct({
+        {0, 1, 2},
+        {128, 256, 512, 1024},
+    })
+    ->Complexity();
+
+template <template <std::size_t, std::size_t> class Grid>
 void BM_RayCasting2d(benchmark::State& state) {
   constexpr double kMaxRange = 100.0;
   constexpr double kResolution = 0.05;
 
-  const auto n = static_cast<int>(state.range(0));
-  auto grid = StaticOccupancyGrid<1280, 1280>{{}, kResolution};
+  const auto bearing_index = static_cast<std::size_t>(state.range(0));
+  const auto n = static_cast<int>(state.range(1));
+  auto grid = Grid<1280, 1280>{{}, kResolution};
   grid.data()[grid.index_at(n, n)] = true;
+  grid.data()[grid.index_at(0, n)] = true;
+  grid.data()[grid.index_at(n, 0)] = true;
 
-  const auto source_pose = Sophus::SE2d{0., Eigen::Vector2d{1., 1.}};
-  const auto beam_bearing = Sophus::SO2d{Sophus::Constants<double>::pi() / 4.};
+  const auto source_pose = Sophus::SE2d{0., Eigen::Vector2d{0., 0.}};
+  const auto beam_bearing = Sophus::SO2d{kBearingAngles.at(bearing_index)};
   const auto beam = beluga::Ray2d{grid, source_pose, kMaxRange};
   for (auto _ : state) {
     benchmark::DoNotOptimize(beam.cast(beam_bearing));
   }
   state.SetComplexityN(n);
+  state.SetLabel(kBearingLabels.at(bearing_index));
 }
 
-BENCHMARK(BM_RayCasting2d)->RangeMultiplier(2)->Range(128, 1024)->Complexity();
+BENCHMARK_TEMPLATE(BM_RayCasting2d, BaselineGrid)
+    ->ArgsProduct({
+        {0, 1, 2},
+        {128, 256, 512, 1024},
+    })
+    ->Complexity();
+
+BENCHMARK_TEMPLATE(BM_RayCasting2d, StaticOccupancyGrid)
+    ->ArgsProduct({
+        {0, 1, 2},
+        {128, 256, 512, 1024},
+    })
+    ->Complexity();
 
 }  // namespace
