02ca4f9f0098c41aeeba4d62b64005934a5fa0eb
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-06-09 12:39:14 -0300

    Add multi-pass guarantee test
    Workaround to test coverage bug.
    
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga/test/beluga/algorithm/raycasting/test_bresenham.cpp
+++ beluga/test/beluga/algorithm/raycasting/test_bresenham.cpp
@@ -24,6 +24,26 @@
 
 namespace beluga {
 
+TEST(Bresenham, MultiPassGuarantee) {
+  // See comparison between iterators in
+  // https://en.cppreference.com/w/cpp/iterator/forward_iterator
+  const auto algorithm = Bresenham2i{Bresenham2i::kStandard};
+
+  const auto line = algorithm({0, 0}, {5, 5});
+  for (auto it1 = line.begin(), it2 = line.begin(); it1 != line.end(); ++it1, ++it2) {
+    ASSERT_EQ(it1, it2);
+  }
+
+  // Test that iterating over the sequence using an independent
+  // iterator doesn't change what an existing iterator refers to.
+  auto it3 = line.begin();
+  ++it3;
+  auto it4 = it3;
+  const auto expected_value = *it3;
+  ++it4;
+  ASSERT_EQ(expected_value, *it3);
+}
+
 TEST(Bresenham, Standard) {
   auto algorithm = Bresenham2i{Bresenham2i::kStandard};
 
