3538b9c29eb396353b41be019a651a21912ed281
Michel Hidalgo <michel@ekumenlabs.com>
2023-06-15 09:22:31 -0300

    Add ROS Noetic support to `beluga_example` (#223)
    Enable `beluga_example` to build against both ROS 2 and ROS 1 distributions.
    
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

---
+++ beluga_example/cmake/catkin.cmake
@@ -0,0 +1,36 @@
+# Copyright 2023 Ekumen, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+find_package(catkin REQUIRED)
+
+catkin_package()
+
+install(DIRECTORY bags
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  PATTERN "*.bag")
+
+install(DIRECTORY launch
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  PATTERN "*.launch")
+
+install(DIRECTORY rviz
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  PATTERN "*.ros.rviz")
+
+install(DIRECTORY params
+  DESTINATION ${CATKIN_PACKAGE_SHARE_DESTINATION}
+  PATTERN "*.ros.yaml")
+
+install(DIRECTORY maps models worlds
+  DESTINATION share/${PROJECT_NAME})
