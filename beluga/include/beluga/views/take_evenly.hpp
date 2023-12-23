d5fbc77fc19256511f666c33f306478ed98e6fe9
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2023-12-23 18:54:20 -0300

    Format to make clang-tidy happy
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

--- beluga/include/beluga/views/take_evenly.hpp
+++ beluga/include/beluga/views/take_evenly.hpp
@@ -37,21 +37,19 @@ struct take_evenly_fn {  // NOLINT(readability-identifier-naming)
    *
    * If `count` or the range size are zero, it returns an empty range.
    * If `count` is greater than the range size, it returns all the elements.
-   * The step size is computed to always include the last element of the range.
+   * The step size is computed to always include the first and last elements of the range.
    */
   template <class Range>
   constexpr auto operator()(Range&& range, std::size_t count) const {
-    // Note: `stride` doesn't support step == 0.
     const std::size_t size = ranges::size(range);
-    if (count == 0 || size == 0) {
-      return range | ranges::views::take(0) | ranges::views::stride(1);
-    } else if (count == 1) {
-      return range | ranges::views::take(1) | ranges::views::stride(1);
-    } else {
-      // Make sure that the last element of the range is included and that the minimum step size is 1.
-      const std::size_t step = std::max(1UL, (size - 1UL) / (count - 1UL));
-      return range | ranges::views::take(size) | ranges::views::stride(step);
+    if (count <= 1UL || size == 0UL) {
+      // Note: We still apply `stride` for return type consistency.
+      return range | ranges::views::take(count) | ranges::views::stride(1);
     }
+
+    // Make sure that the last element of the range is included and the minimum step size is 1.
+    const std::size_t step = std::max(1UL, (size - 1UL) / (count - 1UL));
+    return range | ranges::views::take(size) | ranges::views::stride(step);
   }
 
   /// Overload that returns a view closure to compose with other views.
@@ -60,7 +58,7 @@ struct take_evenly_fn {  // NOLINT(readability-identifier-naming)
    *
    * If `count` or the range size are zero, it returns an empty range.
    * If `count` is greater than the range size, it returns all the elements.
-   * The step size is computed to always include the last element of the range.
+   * The step size is computed to always include the first and last elements of the range.
    */
   constexpr auto operator()(std::size_t count) const {
     return ranges::make_view_closure(ranges::bind_back(take_evenly_fn{}, count));
@@ -76,7 +74,7 @@ struct take_evenly_fn {  // NOLINT(readability-identifier-naming)
  * elements evenly spaced over the source range.
  * If `count` or the range size are zero, it returns an empty range.
  * If `count` is greater than the range size, it returns all the elements.
- * The step size is computed to always include the last element of the range.
+ * The step size is computed to always include the first and last elements of the range.
  */
 inline constexpr detail::take_evenly_fn take_evenly;  // NOLINT(readability-identifier-naming)
 
