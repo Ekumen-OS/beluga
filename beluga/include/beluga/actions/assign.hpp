272fe68aa012adbb5f5659b0e199b6445b3189b9
Nahuel Espinosa <nespinosa@ekumenlabs.com>
2024-01-12 10:23:11 -0300

    Add assign action
    View-action composition in the range library is limited (and often
    produces the wrong result). This patch implements a new range adaptor
    object that can be appended to any view closure to evaluate it
    eagerly and assign the result to a given range, effectively converting
    any view into an action.
    
    It also adds an operator<<= overload to simplify syntax.
    
    Signed-off-by: Nahuel Espinosa <nespinosa@ekumenlabs.com>

---
+++ beluga/include/beluga/actions/assign.hpp
@@ -0,0 +1,116 @@
+// Copyright 2024 Ekumen, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef BELUGA_ACTIONS_ASSIGN_HPP
+#define BELUGA_ACTIONS_ASSIGN_HPP
+
+#include <range/v3/action/action.hpp>
+#include <range/v3/functional/bind_back.hpp>
+#include <range/v3/range/conversion.hpp>
+#include <range/v3/view/view.hpp>
+
+namespace beluga::actions {
+
+/// \cond
+
+template <class T, class = void>
+struct is_range_closure : std::false_type {};
+
+template <class T>
+struct is_range_closure<ranges::actions::action_closure<T>> : std::true_type {};
+
+template <class T>
+struct is_range_closure<ranges::views::view_closure<T>> : std::true_type {};
+
+template <class T>
+inline constexpr bool is_range_closure_v = is_range_closure<T>::value;
+
+/// \endcond
+
+namespace detail {
+
+/// Implementation detail for an assign range adaptor object.
+struct assign_fn {
+  /// Overload that implements the assign algorithm.
+  template <
+      class Range,
+      class Fn,
+      std::enable_if_t<ranges::range<Range>, int> = 0,
+      std::enable_if_t<is_range_closure_v<Fn>, int> = 0>
+  constexpr auto operator()(Range& range, Fn fn) const -> Range& {
+    auto&& view = fn(range);
+    if constexpr (!std::is_same_v<Range, std::decay_t<decltype(view)>>) {
+      // If the result of invoking the closure is not the range itself,
+      // then we need to convert the view and assign it to the input range.
+      range = std::move(view) | ranges::to<Range>;
+    }
+    return range;
+  }
+
+  /// Hidden friend operator overload that enables action / view composition.
+  /**
+   * Enables the following expressions:
+   *   1) view_closure | assign
+   *   2) action_closure | view_closure | assign
+   *   3) action_closure | assign
+   *
+   * 1) Will create an action closure that can eagerly invoke `view_closure` and assign
+   *    the resulting range to the input range.
+   * 2) Will create a new action closure that can invoke `action_closure`, eagerly invoke
+   *    `view_closure`, and assign the resulting range to the input range.
+   * 3) Technically a no-op, it will just invoke the `action_closure`.
+   */
+  template <class Fn, std::enable_if_t<is_range_closure_v<Fn>, int> = 0>
+  friend constexpr auto operator|(Fn fn, assign_fn) {
+    return ranges::make_action_closure(ranges::bind_back(assign_fn{}, std::move(fn)));
+  }
+};
+
+}  // namespace detail
+
+/// [Range adaptor object](https://en.cppreference.com/w/cpp/named_req/RangeAdaptorObject) that
+/// can converts a view closure into an action closure.
+/**
+ * This can be appended to any view closure to evaluate it eagerly and assign the result
+ * to a given range, effectively converting any view into an action.
+ */
+inline constexpr detail::assign_fn assign;
+
+/// Operator overload that appends assign to any range closure.
+template <
+    class Range,
+    class Fn,
+    std::enable_if_t<ranges::range<Range>, int> = 0,
+    std::enable_if_t<is_range_closure_v<Fn>, int> = 0>
+constexpr auto operator<<=(Range& range, Fn fn) -> Range& {
+  return range |= std::move(fn) | beluga::actions::assign;
+}
+
+}  // namespace beluga::actions
+
+// Make the assign operator overload findable by ADL for existing range adaptor objects:
+
+namespace beluga::views {
+using beluga::actions::operator<<=;
+}
+
+namespace ranges::views {
+using beluga::actions::operator<<=;
+}
+
+namespace ranges::actions {
+using beluga::actions::operator<<=;
+}
+
+#endif
---
+++ beluga/test/beluga/actions/test_assign.cpp
@@ -0,0 +1,84 @@
+// Copyright 2024 Ekumen, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <list>
+#include <vector>
+
+#include <gtest/gtest.h>
+
+#include <beluga/actions/assign.hpp>
+
+#include <range/v3/action/drop.hpp>
+#include <range/v3/action/remove.hpp>
+#include <range/v3/algorithm/equal.hpp>
+#include <range/v3/view/reverse.hpp>
+#include <range/v3/view/transform.hpp>
+
+namespace {
+
+TEST(AssignAction, ViewToAction) {
+  auto input = std::vector{1, 2, 3};
+  input |= ranges::views::reverse | beluga::actions::assign;
+  ASSERT_TRUE(ranges::equal(input, std::vector{3, 2, 1}));
+}
+
+TEST(AssignAction, ViewToActionComposition) {
+  auto reverse_and_assign = ranges::views::reverse | beluga::actions::assign;
+  auto input = std::vector{1, 2, 3};
+  input |= reverse_and_assign;
+  ASSERT_TRUE(ranges::equal(input, std::vector{3, 2, 1}));
+}
+
+TEST(AssignAction, ViewToActionCall) {
+  auto input = std::vector{1, 2, 3};
+  beluga::actions::assign(input, ranges::views::reverse);
+  ASSERT_TRUE(ranges::equal(input, std::vector{3, 2, 1}));
+}
+
+TEST(AssignAction, ViewToActionOperator) {
+  auto input = std::vector{1, 2, 3};
+  input <<= ranges::views::reverse;
+  ASSERT_TRUE(ranges::equal(input, std::vector{3, 2, 1}));
+}
+
+TEST(AssignAction, ActionComposition) {
+  auto input = std::vector{1, 2, 3};
+  input |= ranges::actions::drop(1) | beluga::actions::assign;
+  ASSERT_TRUE(ranges::equal(input, std::vector{2, 3}));
+}
+
+TEST(AssignAction, ActionViewComposition) {
+  auto input = std::vector{1, 2, 3};
+  input |= ranges::actions::drop(1) | ranges::views::reverse | beluga::actions::assign;
+  ASSERT_TRUE(ranges::equal(input, std::vector{3, 2}));
+}
+
+TEST(AssignAction, ActionViewActionComposition) {
+  auto input = std::vector{1, 2, 3};
+  input |= ranges::actions::drop(1) |                                      //
+           ranges::views::reverse |                                        //
+           beluga::actions::assign |                                       //
+           ranges::actions::drop(1) |                                      //
+           ranges::views::transform([](auto value) { return ++value; }) |  //
+           beluga::actions::assign;
+  ASSERT_TRUE(ranges::equal(input, std::vector{3}));
+}
+
+TEST(AssignAction, List) {
+  auto input = std::list{1, 2, 3};
+  input <<= ranges::actions::remove(2) | ranges::views::reverse;
+  ASSERT_TRUE(ranges::equal(input, std::list{3, 1}));
+}
+
+}  // namespace
