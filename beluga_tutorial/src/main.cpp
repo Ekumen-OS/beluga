3e4bbd82cbe0b6e6f9def6f9f5346af3ee0fbcbb
Alon Druck <druckalon@gmail.com>
2024-03-25 14:47:23 -0300

    Create beluga_tutorial package (#305)
    Signed-off-by: Alon Druck <alon.druck@ekumenlabs.com>

---
+++ beluga_tutorial/src/main.cpp
@@ -0,0 +1,11 @@
+// https://github.com/Ekumen-OS/beluga/issues/279#issuecomment-1903914387
+
+#include <iostream>
+#include <string>
+
+int main()
+{
+    std::cout << "beluga tutorial - main.cpp executed!!" << std::endl;
+
+    return 0;
+}
