33f15730719b582cab42fab43e2f9b8babb1139e
serraramiro1 <serraramiro@ekumenlabs.com>
2024-04-10 14:35:29 -0300

    Add NDT sensor model (#338)
    ### Proposed changes
    
    Adds a new sensor model, based on NDT transforms, that performs
    importance weighting by transforming the measurements into normal distributions and scoring them against a NDT map.
    
    #### Type of change
    
    - [ ] 🐛 Bugfix (change which fixes an issue)
    - [x] 🚀 Feature (change which adds functionality)
    - [ ] 📚 Documentation (change which fixes or extends documentation)
    
    ---------
    
    Signed-off-by: Ramiro Serra <serraramiro@ekumenlabs.com>

---
+++ beluga/include/beluga/sensor/ndt_sensor_model.hpp
@@ -0,0 +1,242 @@
+// Copyright 2024 Ekumen, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#ifndef BELUGA_SENSOR_NDT_SENSOR_MODEL_HPP
+#define BELUGA_SENSOR_NDT_SENSOR_MODEL_HPP
+
+#include <beluga/sensor/data/ndt_cell.hpp>
+#include <beluga/sensor/data/sparse_value_grid.hpp>
+
+#include <H5Cpp.h>
+#include <sophus/common.hpp>
+#include <sophus/se2.hpp>
+#include <sophus/se3.hpp>
+#include <sophus/so2.hpp>
+
+#include <Eigen/Core>
+
+#include <cstdint>
+#include <filesystem>
+#include <stdexcept>
+#include <type_traits>
+#include <unordered_map>
+
+namespace beluga {
+
+namespace detail {
+/// Hash function for N dimensional Eigen::Vectors of int.
+/// The code is from `hash_combine` function of the Boost library. See
+/// http://www.boost.org/doc/libs/1_55_0/doc/html/hash/reference.html#boost.hash_combine .
+template <int N>
+struct CellHasher {
+  /// Hashes an integer N dimensional integer vector.
+  size_t operator()(const Eigen::Matrix<int, N, 1> vector) const {
+    size_t seed = 0;
+    for (auto i = 0L; i < vector.size(); ++i) {
+      auto elem = *(vector.data() + i);
+      seed ^= std::hash<int>()(elem) + 0x9e3779b9 + (seed << 6) + (seed >> 2);
+    }
+    return seed;
+  }
+};
+
+/// Fit a vector of points to an NDT cell, by computing its mean and covariance.
+template <int NDim, typename Scalar = double>
+inline NDTCell<NDim, Scalar> fit_points(const std::vector<Eigen::Matrix<Scalar, NDim, 1>>& points) {
+  static constexpr double kMinVariance = 1e-5;
+  Eigen::Map<const Eigen::Matrix<Scalar, NDim, Eigen::Dynamic>> points_view(
+      reinterpret_cast<const Scalar*>(points.data()), 2, static_cast<int64_t>(points.size()));
+  const Eigen::Matrix<Scalar, NDim, 1> mean = points_view.rowwise().mean();
+  const Eigen::Matrix<Scalar, NDim, Eigen::Dynamic> centered = points_view.colwise() - mean;
+  // Use sample covariance.
+  Eigen::Matrix<Scalar, NDim, Eigen::Dynamic> cov =
+      (centered * centered.transpose()) / static_cast<double>(points_view.cols() - 1);
+  cov(0, 0) = std::max(cov(0, 0), kMinVariance);
+  cov(1, 1) = std::max(cov(1, 1), kMinVariance);
+  if constexpr (NDim == 3) {
+    cov(2, 2) = std::max(cov(2, 2), kMinVariance);
+  }
+  return NDTCell<NDim, Scalar>{mean, cov};
+}
+
+/// Given a number of N dimensional points and a resolution, constructs a vector of NDT cells, by clusterizing the
+/// points using 'resolution' and fitting a normal distribution to each of the resulting clusters if they contain a
+/// minimum number of points in them.
+template <int NDim, typename Scalar = double>
+inline std::vector<NDTCell<NDim, Scalar>> to_cells(
+    const std::vector<Eigen::Matrix<Scalar, NDim, 1>>& points,
+    const double resolution) {
+  static constexpr int kMinPointsPerCell = 5;
+
+  Eigen::Map<const Eigen::Matrix<Scalar, NDim, Eigen::Dynamic>> points_view(
+      reinterpret_cast<const Scalar*>(points.data()), NDim, static_cast<int64_t>(points.size()));
+
+  std::vector<NDTCell<NDim, Scalar>> ret;
+  ret.reserve(static_cast<size_t>(points_view.cols()) / kMinPointsPerCell);
+
+  std::unordered_map<Eigen::Matrix<int, NDim, 1>, std::vector<Eigen::Matrix<Scalar, NDim, 1>>, CellHasher<NDim>>
+      cell_grid;
+  for (const Eigen::Matrix<Scalar, NDim, 1>& col : points) {
+    cell_grid[(col / resolution).template cast<int>()].emplace_back(col);
+  }
+
+  for (const auto& [cell, points_in_cell] : cell_grid) {
+    if (points_in_cell.size() < kMinPointsPerCell) {
+      continue;
+    }
+    ret.push_back(fit_points<NDim, Scalar>(points_in_cell));
+  }
+
+  return ret;
+}
+
+}  // namespace detail
+
+/// Parameters used to construct a NDTSensorModel instance.
+struct NDTModelParam {
+  /// Likelihood for measurements that lie inside cells that are not present in the map.
+  double minimum_likelihood = 0;
+  /// Scaling parameter d1 in literature, used for scaling 2D likelihoods.
+  double d1 = 1.0;
+  /// Scaling parameter d2 in literature, used for scaling 2D likelihoods.
+  double d2 = 1.0;
+};
+
+/// NDT sensor model for range finders.
+/**
+ * This class satisfies \ref SensorModelPage.
+ *
+ * \tparam SparseGridT Type representing a sparse NDT grid, as a specialization of 'beluga::SparseValueGrid'.
+ */
+template <typename SparseGridT>
+class NDTSensorModel {
+ public:
+  static_assert(std::is_same_v<SparseValueGrid<typename SparseGridT::map_type>, SparseGridT>);
+  /// NDT Cell type.
+  using ndt_cell_type = typename SparseGridT::mapped_type;
+  static_assert(ndt_cell_type::num_dim == 2, "NDT sensor model is only implemented for 2D problems.");
+  /// State type of a particle.
+  using state_type = std::conditional_t<
+      ndt_cell_type::num_dim == 2,
+      Sophus::SE2<typename ndt_cell_type::scalar_type>,
+      Sophus::SE3<typename ndt_cell_type::scalar_type>>;
+  /// Weight type of the particle.
+  using weight_type = double;
+  /// Measurement type of the sensor: a point cloud for the range finder.
+  using measurement_type = std::vector<Eigen::Matrix<typename ndt_cell_type::scalar_type, ndt_cell_type::num_dim, 1>>;
+  /// Map representation type.
+  using map_type = SparseGridT;
+  /// Parameter type that the constructor uses to configure the NDT sensor model.
+  using param_type = NDTModelParam;
+
+  /// Constructs a NDTSensorModel instance.
+  /**
+   * \param params Parameters to configure this instance.
+   *  See beluga::NDTModelParam for details.
+   * \param cells_data Sparse grid representing an NDT map, used for calculating importance weights for the different
+   * particles.
+   */
+  NDTSensorModel(param_type params, SparseGridT cells_data)
+      : params_{std::move(params)}, cells_data_{std::move(cells_data)} {}
+
+  /// Returns a state weighting function conditioned on 2D / 3D lidar hits.
+  /**
+   * \param points 2D lidar hit points in the reference frame of particle states.
+   * \return a state weighting function satisfying \ref StateWeightingFunctionPage
+   *  and borrowing a reference to this sensor model (and thus their lifetime are bound).
+   */
+  [[nodiscard]] auto operator()(measurement_type&& points) const {
+    return [this, cells = detail::to_cells<ndt_cell_type::num_dim, typename ndt_cell_type::scalar_type>(
+                      points, cells_data_.resolution())](const state_type& state) -> weight_type {
+      return std::transform_reduce(
+          cells.cbegin(), cells.cend(), 1.0, std::plus{},
+          [this, state](const ndt_cell_type& ndt_cell) { return likelihood_at(state * ndt_cell); });
+    };
+  }
+
+  /// Returns the L2 likelihood scaled by 'd1' and 'd2' set in the parameters for this instance for 'measurement, or
+  /// 'params_.min_likelihood' if the cell corresponding to 'measurement' doesn't exist in the map.
+  [[nodiscard]] double likelihood_at(const ndt_cell_type& measurement) const {
+    const auto maybe_cell = cells_data_.data_near(measurement.mean);
+    if (!maybe_cell.has_value()) {
+      return params_.minimum_likelihood;
+    }
+    return maybe_cell->likelihood_at(measurement, params_.d1, params_.d2);
+  }
+
+ private:
+  const param_type params_;
+  const SparseGridT cells_data_;
+};
+
+namespace io {
+
+/// Loads a 2D NDT map representation from a hdf5 file, with the following datasets:
+/// - "resolution": () resolution for the discrete grid (cells are resolution x
+///   resolution m^2 squares).
+/// - "cells": (NUM_CELLS, 2) that contains the cell coordinates.
+/// - "means": (NUM_CELLS, 2) that contains the 2d mean of the normal distribution
+///   of the cell.
+/// - "covariances": (NUM_CELLS, 2, 2) Contains the covariance for each cell.
+///
+///  \tparam NDTMapRepresentation A specialized SparseValueGrid (see sensor/data/sparse_value_grid.hpp), where
+///  mapped_type == NDTCell2d, that will represent the NDT map as a mapping from 2D cells to NDTCells.
+template <typename NDTMapRepresentationT>
+NDTMapRepresentationT load_from_hdf5_2d(const std::filesystem::path& path_to_hdf5_file) {
+  static_assert(std::is_same_v<typename NDTMapRepresentationT::mapped_type, NDTCell2d>);
+  static_assert(std::is_same_v<typename NDTMapRepresentationT::key_type, Eigen::Vector2i>);
+  if (!std::filesystem::exists(path_to_hdf5_file) || std::filesystem::is_directory(path_to_hdf5_file)) {
+    std::stringstream ss;
+    ss << "Couldn't find a valid HDF5 file at " << path_to_hdf5_file;
+    throw std::invalid_argument(ss.str());
+  }
+
+  H5::H5File file(path_to_hdf5_file, H5F_ACC_RDONLY);
+  H5::DataSet means_dataset = file.openDataSet("means");
+  H5::DataSet covariances_dataset = file.openDataSet("covariances");
+  H5::DataSet resolution_dataset = file.openDataSet("resolution");
+  H5::DataSet cells_dataset = file.openDataSet("cells");
+
+  std::array<hsize_t, 2> dims_out;
+  means_dataset.getSpace().getSimpleExtentDims(dims_out.data(), nullptr);
+
+  Eigen::Matrix2Xd means_matrix(dims_out[1], dims_out[0]);
+  Eigen::Matrix2Xi cells_matrix(dims_out[1], dims_out[0]);
+
+  means_dataset.read(means_matrix.data(), H5::PredType::NATIVE_DOUBLE);
+  cells_dataset.read(cells_matrix.data(), H5::PredType::NATIVE_INT);
+
+  std::vector<Eigen::Array<double, 2, 2>> covariances(dims_out[0]);
+  covariances_dataset.read(covariances.data(), H5::PredType::NATIVE_DOUBLE);
+
+  double resolution;
+
+  resolution_dataset.read(&resolution, H5::PredType::NATIVE_DOUBLE);
+
+  typename NDTMapRepresentationT::map_type map{};
+
+  // Note: Ranges::zip_view doesn't seem to work in old Eigen.
+  for (size_t i = 0; i < covariances.size(); ++i) {
+    map[cells_matrix.col(static_cast<Eigen::Index>(i))] =
+        NDTCell2d{means_matrix.col(static_cast<Eigen::Index>(i)), covariances.at(i)};
+  }
+
+  return NDTMapRepresentationT{std::move(map), resolution};
+}
+
+}  // namespace io
+
+}  // namespace beluga
+
+#endif
---
+++ beluga/test/beluga/sensor/test_ndt_model.cpp
@@ -0,0 +1,166 @@
+// Copyright 2024 Ekumen, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdexcept>
+#include <unordered_map>
+
+#include <beluga/sensor/data/sparse_value_grid.hpp>
+#include <beluga/sensor/ndt_sensor_model.hpp>
+
+#include <Eigen/Core>
+
+#include <gmock/gmock.h>
+#include <gtest/gtest-typed-test.h>
+#include <gtest/gtest.h>
+#include <sophus/se2.hpp>
+
+namespace beluga {
+
+namespace {
+
+Eigen::Matrix2Xd get_diagonal_covariance(double x_var = 0.5, double y_var = 0.5) {
+  return Eigen::Vector2d{x_var, y_var}.asDiagonal();
+}
+using sparse_grid_2d_t = SparseValueGrid<std::unordered_map<Eigen::Vector2i, NDTCell2d, detail::CellHasher<2>>>;
+}  // namespace
+
+TEST(NDTSensorModelTests, CanConstruct) {
+  NDTSensorModel{{}, sparse_grid_2d_t{}};
+}
+
+TEST(NDTSensorModelTests, MinLikelihood) {
+  const double minimum_likelihood = -1.0;
+
+  NDTModelParam param{minimum_likelihood};
+  NDTSensorModel model{param, sparse_grid_2d_t{}};
+
+  for (const auto& val : {
+           Eigen::Vector2d{0.1, 0.1},
+           Eigen::Vector2d{0.15, 0.15},
+           Eigen::Vector2d{0.25, 0.25},
+           Eigen::Vector2d{0.35, 0.35},
+           Eigen::Vector2d{0.45, 0.45},
+           Eigen::Vector2d{0.50, 0.50},
+           Eigen::Vector2d{1.65, 0.65},
+           Eigen::Vector2d{0.75, 0.75},
+
+       }) {
+    ASSERT_DOUBLE_EQ(model.likelihood_at({val, get_diagonal_covariance()}), minimum_likelihood);
+  }
+}
+
+TEST(NDTSensorModelTests, Likelihoood) {
+  typename sparse_grid_2d_t::map_type map;
+  {
+    Eigen::Array<double, 2, 2> cov;
+    // clang-format off
+    cov << 0.5, 0.0,
+           0.0, 0.3;
+    // clang-format on
+    Eigen::Vector2d mean(0.5, 0.5);
+    map[Eigen::Vector2i(0, 0)] = NDTCell2d{mean, cov};
+  }
+  {
+    Eigen::Array<double, 2, 2> cov;
+    // clang-format off
+    cov << 0.5, 0.0,
+           0.0, 0.5;
+    // clang-format on
+    Eigen::Vector2d mean(1.5, 1.5);
+    map[Eigen::Vector2i(1, 1)] = NDTCell2d{mean, cov};
+  }
+  sparse_grid_2d_t grid{std::move(map), 1.0};
+
+  const double minimum_likelihood = -1.0;
+  NDTModelParam param{minimum_likelihood};
+  NDTSensorModel model{param, std::move(grid)};
+
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{0.5, 0.5}, get_diagonal_covariance()}), 1);
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{0.8, 0.5}, get_diagonal_covariance()}), 0.95599748183309985);
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{0.5, 0.8}, get_diagonal_covariance()}), 0.94530278065205942);
+
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{1.5, 1.5}, get_diagonal_covariance()}), 1);
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{1.8, 1.5}, get_diagonal_covariance()}), 0.95599748183309985);
+  EXPECT_DOUBLE_EQ(model.likelihood_at({{1.5, 1.8}, get_diagonal_covariance()}), 0.95599748183309985);
+}
+
+TEST(NDTSensorModelTests, FitPoints) {
+  {
+    std::vector meas{
+        Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.2},
+        Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.2},
+    };
+    auto cell = detail::fit_points<2>(meas);
+    ASSERT_TRUE(cell.mean.isApprox(Eigen::Vector2d{0.1, 0.2}));
+    // We introduce a minimum variance to avoid numeric errors down the line.
+    ASSERT_FALSE(cell.covariance.isZero());
+  }
+  {
+    std::vector meas{
+        Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.9}, Eigen::Vector2d{0.1, 0.2},
+        Eigen::Vector2d{0.1, 0.9}, Eigen::Vector2d{0.1, 0.2}, Eigen::Vector2d{0.1, 0.2},
+    };
+    auto cell = detail::fit_points<2>(meas);
+    ASSERT_TRUE(cell.mean.isApprox(Eigen::Vector2d{0.1, 0.433333}, 1e-6));
+    ASSERT_FALSE(cell.covariance.isZero());
+    ASSERT_GT(cell.covariance(1, 1), cell.covariance(0, 0));
+  }
+}
+
+TEST(NDTSensorModelTests, ToCellsNotEnoughPointsInCell) {
+  const double map_res = 0.5;
+  std::vector map_data{
+      Eigen::Vector2d{0.1, 0.2},
+      Eigen::Vector2d{0.112, 0.22},
+      Eigen::Vector2d{0.15, 0.23},
+  };
+  const auto cells = detail::to_cells<2UL>(map_data, map_res);
+  ASSERT_EQ(cells.size(), 0UL);
+}
+
+TEST(NDTSensorModelTests, SensorModel) {
+  double map_res = 0.5;
+  std::vector map_data{
+      Eigen::Vector2d{0.1, 0.2},  Eigen::Vector2d{0.112, 0.22}, Eigen::Vector2d{0.15, 0.23},
+      Eigen::Vector2d{0.1, 0.24}, Eigen::Vector2d{0.16, 0.25},  Eigen::Vector2d{0.1, 0.26},
+  };
+  auto cells = detail::to_cells<2UL>(map_data, map_res);
+
+  typename sparse_grid_2d_t::map_type map_cells_data;
+  for (const auto& cell : cells) {
+    map_cells_data[(cell.mean.array() / map_res).cast<int>()] = cell;
+  }
+  std::vector perfect_measurement = map_data;
+  NDTSensorModel model{{}, sparse_grid_2d_t{map_cells_data, map_res}};
+  auto state_weighing_fn = model(std::move(perfect_measurement));
+  // This is a perfect hit, so we should expect weight to be 1 + num_cells == 2.
+  ASSERT_DOUBLE_EQ(state_weighing_fn(Sophus::SE2d{}), 2);
+
+  // Subtle miss, this value should be close to 1 but not quite.
+  ASSERT_GT(state_weighing_fn(Sophus::SE2d{Sophus::SO2d{}, Eigen::Vector2d{0.1, 0.1}}), 1.0);
+
+  // This is a perfect miss, so we should expect weight to be 1.
+  ASSERT_DOUBLE_EQ(state_weighing_fn(Sophus::SE2d{Sophus::SO2d{}, Eigen::Vector2d{-10, -10}}), 1.0);
+}
+
+TEST(NDTSensorModelTests, LoadFromHDF5HappyPath) {
+  const auto ndt_map_representation = io::load_from_hdf5_2d<sparse_grid_2d_t>("./test_data/turtlebot3_world.hdf5");
+  ASSERT_EQ(ndt_map_representation.size(), 30UL);
+}
+
+TEST(NDTSensorModelTests, LoadFromHDF5NonExistingFile) {
+  ASSERT_THROW(io::load_from_hdf5_2d<sparse_grid_2d_t>("bad_file.hdf5"), std::invalid_argument);
+}
+
+}  // namespace beluga
