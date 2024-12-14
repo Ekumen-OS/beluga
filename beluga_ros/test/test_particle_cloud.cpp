cd0b677f367ef3646dfc51e13c3f6d785e770f10
Pablo Vela <pablovela@naver.com>
2024-12-14 17:46:11 +0800

    Add preliminary tests
    Signed-off-by: Pablo Vela <pablovela@naver.com>

--- beluga_ros/test/test_particle_cloud.cpp
+++ beluga_ros/test/test_particle_cloud.cpp
@@ -23,6 +23,7 @@
 
 #include <sophus/common.hpp>
 #include <sophus/se2.hpp>
+#include <sophus/se3.hpp>
 
 #include "beluga/primitives.hpp"
 #include "beluga_ros/messages.hpp"
@@ -48,13 +49,44 @@ TEST(TestParticleCloud, Assign) {
     EXPECT_DOUBLE_EQ(pose.position.z, 0.0);
     EXPECT_DOUBLE_EQ(pose.orientation.x, 0.0);
     EXPECT_DOUBLE_EQ(pose.orientation.y, 0.0);
-    EXPECT_DOUBLE_EQ(pose.orientation.x, 0.0);
-    EXPECT_DOUBLE_EQ(pose.orientation.y, 0.0);
     EXPECT_DOUBLE_EQ(std::abs(pose.orientation.z), std::sin(Constants::pi() / 4.0));
     EXPECT_DOUBLE_EQ(pose.orientation.w, std::cos(Constants::pi() / 4.0));
   }
-  const auto num_particles_in_upper_half_xy_plane =
+  const auto num_particles_in_upper_half_xz_plane =
       ranges::count_if(message.poses, [](const auto& pose) { return pose.position.y > 0.0; });
+  const double upper_half_xz_plane_weight =
+      static_cast<double>(num_particles_in_upper_half_xz_plane) / static_cast<double>(message.poses.size());
+  EXPECT_NEAR(upper_half_xz_plane_weight, 0.5, kSampleSize * 0.01);  // allow 1% deviations
+}
+
+TEST(TestParticleCloud3, Assign) {
+  const auto particles = std::vector{
+      std::make_tuple(
+          Sophus::SE3d{
+              Sophus::SO3d{Eigen::Quaternion<double>{0.8497105, 0.0, 0.3728217, 0.3728217}},
+              Eigen::Vector3d{0.0, 0.0, 1.0}},
+          beluga::Weight(0.5)),
+      std::make_tuple(
+          Sophus::SE3d{
+              Sophus::SO3d{Eigen::Quaternion<double>{0.8497105, 0.0, -0.3728217, 0.3728217}},
+              Eigen::Vector3d{0.0, 0.0, -1.0}},
+          beluga::Weight(0.5)),
+  };
+  constexpr std::size_t kSampleSize = 1000U;
+  auto message = beluga_ros::msg::PoseArray{};
+  beluga_ros::assign_particle_cloud(particles, kSampleSize, message);
+  EXPECT_EQ(message.poses.size(), kSampleSize);
+  for (const auto& pose : message.poses) {
+    EXPECT_DOUBLE_EQ(pose.position.x, 0.0);
+    EXPECT_DOUBLE_EQ(pose.position.y, 0.0);
+    EXPECT_DOUBLE_EQ(std::abs(pose.position.z), 1.0);
+    EXPECT_DOUBLE_EQ(pose.orientation.x, 0.0);
+    EXPECT_NEAR(std::abs(pose.orientation.y), 0.3728217, 0.001);
+    EXPECT_NEAR(pose.orientation.z, 0.3728217, 0.001);
+    EXPECT_NEAR(pose.orientation.w, 0.8497105, 0.001);
+  }
+  const auto num_particles_in_upper_half_xy_plane =
+      ranges::count_if(message.poses, [](const auto& pose) { return pose.position.z > 0.0; });
   const double upper_half_xy_plane_weight =
       static_cast<double>(num_particles_in_upper_half_xy_plane) / static_cast<double>(message.poses.size());
   EXPECT_NEAR(upper_half_xy_plane_weight, 0.5, kSampleSize * 0.01);  // allow 1% deviations
@@ -63,6 +95,15 @@ TEST(TestParticleCloud, Assign) {
 TEST(TestParticleCloud, AssignNone) {
   const auto particles = std::vector{
       std::make_tuple(Sophus::SE2d{}, beluga::Weight(1.0)),
+  };
+  auto message = beluga_ros::msg::PoseArray{};
+  beluga_ros::assign_particle_cloud(particles, 0U, message);
+  EXPECT_EQ(message.poses.size(), 0U);
+}
+
+TEST(TestParticleCloud3, AssignNone) {
+  const auto particles = std::vector{
+      std::make_tuple(Sophus::SE3d{}, beluga::Weight(1.0)),
   };
   auto message = beluga_ros::msg::PoseArray{};
   beluga_ros::assign_particle_cloud(particles, 0U, message);
@@ -78,8 +119,24 @@ TEST(TestParticleCloud, AssignMatchingDistribution) {
   EXPECT_EQ(message.poses.size(), particles.size());
 }
 
+TEST(TestParticleCloud3, AssignMatchingDistribution) {
+  const auto particles = std::vector{
+      std::make_tuple(Sophus::SE3d{}, beluga::Weight(1.0)),
+  };
+  auto message = beluga_ros::msg::PoseArray{};
+  beluga_ros::assign_particle_cloud(particles, message);
+  EXPECT_EQ(message.poses.size(), particles.size());
+}
+
 TEST(TestParticleCloud, AssignMatchingEmpty) {
   const auto particles = std::vector<std::tuple<Sophus::SE2d, beluga::Weight>>{};
+  auto message = beluga_ros::msg::PoseArray{};
+  beluga_ros::assign_particle_cloud(particles, message);
+  EXPECT_EQ(message.poses.size(), 0U);
+}
+
+TEST(TestParticleCloud3, AssignMatchingEmpty) {
+  const auto particles = std::vector<std::tuple<Sophus::SE3d, beluga::Weight>>{};
   auto message = beluga_ros::msg::PoseArray{};
   beluga_ros::assign_particle_cloud(particles, message);
   EXPECT_EQ(message.poses.size(), 0U);
@@ -99,10 +156,32 @@ TEST(TestParticleCloud, AssignMarkers) {
   EXPECT_EQ(message.markers[1].points.size(), 3 * 2);  // 2 arrows, 3 vertices each
 }
 
+TEST(TestParticleCloud3, AssignMarkers) {
+  const auto particles = std::vector{
+      std::make_tuple(Sophus::SE3d{Sophus::SO3d{}, Eigen::Vector3d{1.0, 0.0, 0.0}}, beluga::Weight(0.35)),
+      std::make_tuple(Sophus::SE3d{Sophus::SO3d{}, Eigen::Vector3d{1.0, 0.0, 0.0}}, beluga::Weight(0.25)),
+      std::make_tuple(
+          Sophus::SE3d{Sophus::SO3d{Eigen::Quaternion<double>{1.0, 0.0, 0.0, 0.0}}, Eigen::Vector3d{0.0, -1.0, 0.0}},
+          beluga::Weight(0.4)),
+  };
+  auto message = beluga_ros::msg::MarkerArray{};
+  // beluga_ros::assign_particle_cloud(particles, message);
+  ASSERT_EQ(message.markers.size(), 2U);
+  EXPECT_EQ(message.markers[0].points.size(), 2 * 2);  // 2 arrows, 2 vertices each
+  EXPECT_EQ(message.markers[1].points.size(), 3 * 2);  // 2 arrows, 3 vertices each
+}
+
 TEST(TestParticleCloud, AssignNoMarkers) {
   const auto particles = std::vector<std::tuple<Sophus::SE2d, beluga::Weight>>{};
   auto message = beluga_ros::msg::MarkerArray{};
   beluga_ros::assign_particle_cloud(particles, message);
+  EXPECT_EQ(message.markers.size(), 0U);
+}
+
+TEST(TestParticleCloud3, AssignNoMarkers) {
+  const auto particles = std::vector<std::tuple<Sophus::SE3d, beluga::Weight>>{};
+  auto message = beluga_ros::msg::MarkerArray{};
+  // beluga_ros::assign_particle_cloud(particles, message);
   EXPECT_EQ(message.markers.size(), 0U);
 }
 
