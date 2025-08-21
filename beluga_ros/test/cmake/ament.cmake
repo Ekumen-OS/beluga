1444c6a41922e794f3ed1d9b67350a9973b10e61
Xavier Ruiz <xavier.ruiz@ekumenlabs.com>
2025-08-21 13:02:38 +0200

    add support for isolated tests using ament_cmake_ros and drop unused test depend ament_cmake_gtest
    Signed-off-by: Xavier Ruiz <xavier.ruiz@ekumenlabs.com>

--- beluga_ros/test/cmake/ament.cmake
+++ beluga_ros/test/cmake/ament.cmake
@@ -12,33 +12,33 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-find_package(ament_cmake_gmock REQUIRED)
+find_package(ament_cmake_ros REQUIRED)
 find_package(sensor_msgs REQUIRED)
 
-ament_add_gmock(test_amcl test_amcl.cpp)
+ament_add_ros_isolated_gmock(test_amcl test_amcl.cpp)
 target_compile_options(test_amcl PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_amcl beluga_ros)
 
-ament_add_gmock(test_messages test_messages.cpp)
+ament_add_ros_isolated_gmock(test_messages test_messages.cpp)
 target_compile_options(test_messages PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_messages beluga_ros)
 
-ament_add_gmock(test_occupancy_grid test_occupancy_grid.cpp)
+ament_add_ros_isolated_gmock(test_occupancy_grid test_occupancy_grid.cpp)
 target_compile_options(test_occupancy_grid PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_occupancy_grid beluga_ros)
 
-ament_add_gmock(test_tf2_sophus test_tf2_sophus.cpp)
+ament_add_ros_isolated_gmock(test_tf2_sophus test_tf2_sophus.cpp)
 target_compile_options(test_tf2_sophus PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_tf2_sophus beluga_ros)
 
-ament_add_gmock(test_laser_scan test_laser_scan.cpp)
+ament_add_ros_isolated_gmock(test_laser_scan test_laser_scan.cpp)
 target_compile_options(test_laser_scan PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_laser_scan beluga_ros)
 
-ament_add_gmock(test_particle_cloud test_particle_cloud.cpp)
+ament_add_ros_isolated_gmock(test_particle_cloud test_particle_cloud.cpp)
 target_compile_options(test_particle_cloud PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_particle_cloud beluga_ros)
 
-ament_add_gmock(test_point_cloud test_point_cloud.cpp)
+ament_add_ros_isolated_gmock(test_point_cloud test_point_cloud.cpp)
 target_compile_options(test_point_cloud PRIVATE -Wno-deprecated-copy)
 target_link_libraries(test_point_cloud beluga_ros)
