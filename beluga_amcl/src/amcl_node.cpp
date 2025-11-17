2696cd188dcba73ca25456e6387b29c2b5e4f605
Andrés Brumovsky <andres.brumovsky@ekumenlabs.com>
2025-11-17 14:20:36 -0300

    Deal with deprecated headers in Rolling (#531)
    Weekly CI failed on Rolling due to warning on headers deprecation
    
    ### Proposed changes
    
    Update header and check back compatibility with other distros
    
    #### Type of change
    
    - [x ] 🐛 Bugfix (change which fixes an issue)
    - [ ] 🚀 Feature (change which adds functionality)
    - [ ] 📚 Documentation (change which fixes or extends documentation)
    
    💥 **Breaking change!** _Explain why a non-backwards compatible change is
    necessary or remove this line entirely if not applicable._
    
    ### Checklist
    
    _Put an `x` in the boxes that apply. This is simply a reminder of what
    we will require before merging your code._
    
    - [ ] Lint and unit tests (if any) pass locally with my changes
    - [ ] I have added tests that prove my fix is effective or that my
    feature works
    - [ ] I have added necessary documentation (if appropriate)
    - [ ] All commits have been signed for
    [DCO](https://developercertificate.org/)
    
    ### Additional comments
    
    _Anything worth mentioning to the reviewers._
    
    ---------
    
    Signed-off-by: Bruma <andres.brumovsky@ekumenlabs.com>

--- beluga_amcl/src/amcl_node.cpp
+++ beluga_amcl/src/amcl_node.cpp
@@ -30,12 +30,12 @@
 #include <tf2/exceptions.hpp>
 #include <tf2/time.hpp>
 
-#include <tf2_ros/buffer.h>
-#include <tf2_ros/buffer_interface.h>
-#include <tf2_ros/create_timer_ros.h>
-#include <tf2_ros/message_filter.h>
-#include <tf2_ros/transform_broadcaster.h>
-#include <tf2_ros/transform_listener.h>
+#include <tf2_ros/buffer.hpp>
+#include <tf2_ros/buffer_interface.hpp>
+#include <tf2_ros/create_timer_ros.hpp>
+#include <tf2_ros/message_filter.hpp>
+#include <tf2_ros/transform_broadcaster.hpp>
+#include <tf2_ros/transform_listener.hpp>
 
 #include <Eigen/Core>
 #include <sophus/se2.hpp>
