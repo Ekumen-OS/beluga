b70c4b059c67ca056251cc2ca8eaf6e5aa90c399
Michel Hidalgo <michel@ekumenlabs.com>
2025-11-25 14:39:39 -0300

    Align package versions
    Signed-off-by: Michel Hidalgo <michel@ekumenlabs.com>

--- beluga_tools/setup.py
+++ beluga_tools/setup.py
@@ -18,7 +18,7 @@ package_name = 'beluga_tools'
 
 setup(
     name=package_name,
-    version='0.0.0',
+    version='2.0.2',
     packages=find_packages(exclude=['test']),
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
