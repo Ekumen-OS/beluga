92096e12c08e9724f0192fa3bd3fd38b707fe465
Jesús Silva <79662866+JesusSilvaUtrera@users.noreply.github.com>
2026-02-03 15:53:19 +0100

    Add new overload for reweight action that tracks likelihood (#530)
    ### Proposed changes
    
    Closes #524. Changes include a new overload for the `reweight` action,
    tracking likelihood as well and storing it in the particle structure if
    there is a member for it.
    
    #### Type of change
    
    - [ ] 🐛 Bugfix (change which fixes an issue)
    - [x] 🚀 Feature (change which adds functionality)
    - [ ] 📚 Documentation (change which fixes or extends documentation)
    
    ### Checklist
    
    _Put an `x` in the boxes that apply. This is simply a reminder of what
    we will require before merging your code._
    
    - [x] Lint and unit tests (if any) pass locally with my changes
    - [x] I have added tests that prove my fix is effective or that my
    feature works
    - [x] I have added necessary documentation (if appropriate)
    - [x] All commits have been signed for
    [DCO](https://developercertificate.org/)
    
    ### Additional comments
    
    _Anything worth mentioning to the reviewers._
    
    Signed-off-by: JesusSilvaUtrera <jesus.silva@ekumenlabs.com>

--- beluga/include/beluga/actions/reweight.hpp
+++ beluga/include/beluga/actions/reweight.hpp
@@ -17,13 +17,14 @@
 
 #include <algorithm>
 #include <execution>
+#include <type_traits>
 
 #include <beluga/type_traits/particle_traits.hpp>
+#include <beluga/views/likelihoods.hpp>
 #include <beluga/views/particles.hpp>
 
 #include <range/v3/action/action.hpp>
-#include <range/v3/algorithm/max_element.hpp>
-#include <range/v3/view/common.hpp>
+#include <range/v3/view/zip.hpp>
 
 /**
  * \file
@@ -34,83 +35,129 @@ namespace beluga::actions {
 
 namespace detail {
 
+/// Internal trait to detect if a particle has a public 'likelihood' member variable.
+template <typename T, typename = void>
+struct has_likelihood_member : std::false_type {};
+
+template <typename T>
+struct has_likelihood_member<T, std::void_t<decltype(std::declval<T&>().likelihood)>> : std::true_type {};
+
+/// Helper constant for the likelihood member trait.
+template <typename T>
+inline constexpr bool has_likelihood_member_v = has_likelihood_member<T>::value;
+
 /// Implementation detail for a reweight range adaptor object.
-struct reweight_base_fn {
-  /// Overload that implements the reweight algorithm.
+struct reweight_fn {
+ private:
+  /// Internal core implementation for reweighting logic.
+  template <class ExecutionPolicy, class Range, class LikelihoodRange>
+  constexpr void apply_impl(ExecutionPolicy&& policy, Range& range, const LikelihoodRange& likelihoods) const {
+    // Update particle weights using std::transform.
+    auto weights = range | beluga::views::weights;
+    std::transform(
+        policy, std::begin(weights), std::end(weights), std::begin(likelihoods), std::begin(weights),
+        [](auto weight, auto likelihood) { return weight * likelihood; });
+
+    // Store raw likelihood if the particle type has storage for it.
+    // We use ranges::views::zip here because we are performing side-effects (writing to members)
+    // which std::transform is not designed for.
+    using ParticleType = ranges::range_value_t<Range>;
+    if constexpr (has_likelihood_member_v<ParticleType>) {
+      auto zipped = ranges::views::zip(range, likelihoods);
+      std::for_each(policy, std::begin(zipped), std::end(zipped), [](auto&& tuple) {
+        auto& particle = std::get<0>(tuple);
+        particle.likelihood = std::get<1>(tuple);
+      });
+    }
+  }
+
+ public:
+  /// Primary overload that applies a given range of likelihoods or a model to a particle range.
   /**
    * \tparam ExecutionPolicy An [execution policy](https://en.cppreference.com/w/cpp/algorithm/execution_policy_tag_t).
    * \tparam Range An [input range](https://en.cppreference.com/w/cpp/ranges/input_range) of particles.
-   * \tparam Model A callable that can compute the importance weight given a particle state.
+   * \tparam Input Either a LikelihoodRange or a Model callable.
    * \param policy The execution policy to use.
    * \param range An existing range of particles to apply this action to.
-   * \param model A callable instance to compute the weights given the particle states.
-   *
-   * For each particle, we multiply the current weight by the new importance weight to accumulate information from
-   * sensor updates.
+   * \param input A range of likelihood values or a sensor model to calculate them.
    */
   template <
       class ExecutionPolicy,
       class Range,
-      class Model,
+      class Input,
       std::enable_if_t<std::is_execution_policy_v<std::decay_t<ExecutionPolicy>>, int> = 0,
       std::enable_if_t<ranges::range<Range>, int> = 0>
-  constexpr auto operator()(ExecutionPolicy&& policy, Range& range, Model model) const -> Range& {
+  constexpr auto operator()(ExecutionPolicy&& policy, Range& range, Input&& input) const -> Range& {
     static_assert(beluga::is_particle_range_v<Range>);
-    auto states = range | beluga::views::states | ranges::views::common;
-    auto weights = range | beluga::views::weights | ranges::views::common;
-    std::transform(
-        policy,               //
-        std::begin(states),   //
-        std::end(states),     //
-        std::begin(weights),  //
-        std::begin(weights),  //
-        [model = std::move(model)](const auto& s, auto w) { return w * model(s); });
+    if constexpr (ranges::range<Input>) {
+      apply_impl(std::forward<ExecutionPolicy>(policy), range, input);
+    } else {
+      apply_impl(
+          std::forward<ExecutionPolicy>(policy), range, range | beluga::views::likelihoods(std::forward<Input>(input)));
+    }
     return range;
   }
 
-  /// Overload that re-orders arguments from a view closure.
+  /// Convenience overload that defines a default execution policy.
+  /**
+   * \param range An existing range of particles to apply this action to.
+   * \param input A range of likelihood values or a sensor model to calculate them.
+   */
   template <
       class Range,
-      class Model,
+      class Input,
+      std::enable_if_t<ranges::range<Range>, int> = 0,
+      std::enable_if_t<!std::is_execution_policy_v<std::decay_t<Input>>, int> = 0>
+  constexpr auto operator()(Range& range, Input&& input) const -> Range& {
+    return (*this)(std::execution::seq, range, std::forward<Input>(input));
+  }
+
+  /// Re-ordering overload to support range-v3 closure argument passing.
+  /**
+   * This is required to bridge the gap between piped closures and the primary implementation.
+   */
+  template <
+      class Range,
+      class Input,
       class ExecutionPolicy,
       std::enable_if_t<ranges::range<Range>, int> = 0,
-      std::enable_if_t<std::is_execution_policy_v<ExecutionPolicy>, int> = 0>
-  constexpr auto operator()(Range&& range, Model model, ExecutionPolicy policy) const -> Range& {
-    return (*this)(std::move(policy), std::forward<Range>(range), std::move(model));
+      std::enable_if_t<std::is_execution_policy_v<std::decay_t<ExecutionPolicy>>, int> = 0>
+  constexpr auto operator()(Range&& range, Input&& input, ExecutionPolicy&& policy) const -> auto& {
+    return (*this)(std::forward<ExecutionPolicy>(policy), range, std::forward<Input>(input));
   }
 
-  /// Overload that returns a view closure to compose with other views.
-  template <class ExecutionPolicy, class Model, std::enable_if_t<std::is_execution_policy_v<ExecutionPolicy>, int> = 0>
-  constexpr auto operator()(ExecutionPolicy policy, Model model) const {
-    return ranges::make_action_closure(ranges::bind_back(reweight_base_fn{}, std::move(model), std::move(policy)));
-  }
-};
-
-/// Implementation detail for a reweight range adaptor object with a default execution policy.
-struct reweight_fn : public reweight_base_fn {
-  using reweight_base_fn::operator();
-
-  /// Overload that defines a default execution policy.
-  template <class Range, class Model, std::enable_if_t<ranges::range<Range>, int> = 0>
-  constexpr auto operator()(Range&& range, Model model) const -> Range& {
-    return (*this)(std::execution::seq, std::forward<Range>(range), std::move(model));
+  /// Overload that returns a view closure to compose with other views using an execution policy.
+  /**
+   * \param policy The execution policy to use.
+   * \param input A range of likelihood values or a sensor model to calculate them.
+   */
+  template <
+      class ExecutionPolicy,
+      class Input,
+      std::enable_if_t<std::is_execution_policy_v<std::decay_t<ExecutionPolicy>>, int> = 0,
+      std::enable_if_t<!ranges::range<std::decay_t<ExecutionPolicy>>, int> = 0>
+  constexpr auto operator()(ExecutionPolicy policy, Input input) const {
+    return ranges::make_action_closure(ranges::bind_back(reweight_fn{}, std::move(input), std::move(policy)));
   }
 
-  /// Overload that returns a view closure to compose with other views.
-  template <class Model>
-  constexpr auto operator()(Model model) const {
-    return ranges::make_action_closure(ranges::bind_back(reweight_fn{}, std::move(model)));
+  /// Overload that returns a view closure to compose with other views using the default execution policy.
+  /**
+   * \param input A range of likelihood values or a sensor model to calculate them.
+   */
+  template <class Input, std::enable_if_t<!std::is_execution_policy_v<std::decay_t<Input>>, int> = 0>
+  constexpr auto operator()(Input input) const {
+    return ranges::make_action_closure(ranges::bind_back(reweight_fn{}, std::move(input)));
   }
 };
 
 }  // namespace detail
 
 /// [Range adaptor object](https://en.cppreference.com/w/cpp/named_req/RangeAdaptorObject) that
-/// can update the weights in a particle range using a sensor model.
+/// updates the weights in a particle range using a sensor model or a likelihood range.
 /**
  * This action updates particle weights by importance weight multiplication.
- * These importance weights are computed by a given measurement likelihood
- * function (or sensor model) for current particle states.
+ * If the particle type has a public `likelihood` member, the raw likelihood values
+ * used for the update are also stored in the particles.
  */
 inline constexpr detail::reweight_fn reweight;
 
